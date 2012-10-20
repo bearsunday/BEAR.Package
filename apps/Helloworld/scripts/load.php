d221800c9a959c248561005668623156d2f4eb51
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-21 04:06:19 +0900

    Fix loader.

--- apps/Helloworld/scripts/load.php
+++ apps/Helloworld/scripts/load.php
@@ -2,6 +2,7 @@
 namespace Helloworld;
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\AnnotationReader;
 
 $packageDir = dirname(dirname(dirname(__DIR__)));
 
@@ -9,10 +10,13 @@ $packageDir = dirname(dirname(dirname(__DIR__)));
 require_once $packageDir . '/vendor/autoload.php';
 
 // Core file loader
-require_once $packageDir . '/vendor/bear/sunday/scripts/core_loader.php';
+require_once $packageDir . '/scripts/core_loader.php';
 
 // Annotation auto loader
 AnnotationRegistry::registerAutoloadNamespace(__NAMESPACE__ . '\Annotation\\', dirname(__DIR__));
 AnnotationRegistry::registerAutoloadNamespace('Ray\Di\Di\\', $packageDir . '/vendor/ray/di/src/');
 AnnotationRegistry::registerAutoloadNamespace('BEAR\Resource\Annotation\\', $packageDir . '/vendor/bear/resource/src/');
 AnnotationRegistry::registerAutoloadNamespace('BEAR\Sunday\Annotation\\', $packageDir . '/vendor/bear/sunday/src/');
+AnnotationReader::addGlobalIgnoredName('noinspection');
+AnnotationReader::addGlobalIgnoredName('returns'); // for Mr.Smarty. :(
+unset($packageDir);
