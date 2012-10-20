eca5813a9024fea674c9718f1bd3fa0d5fb9e38c
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-21 04:03:20 +0900

    Cache app object.

--- apps/Helloworld/scripts/instance.php
+++ apps/Helloworld/scripts/instance.php
@@ -4,12 +4,17 @@
  *
  * @package App.Sandbox
  */
-namespace Helloworld;
 
 use Helloworld\Module\AppModule;
 use Ray\Di\Injector;
 
+$app = apc_fetch('app-helloworld');
+if ($app) {
+    return $app;
+}
+
 $injector = Injector::create([new AppModule], true);
 $app = $injector->getInstance('BEAR\Sunday\Application\Context');
 
+apc_store('app-helloworld', $app);
 return $app;
