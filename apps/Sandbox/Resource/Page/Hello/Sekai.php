6f234ae0442913d91f10a04a3d9b5f87ee72af34
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-10-25 03:16:10 +0900

    .htaccsess

---
+++ apps/Sandbox/Resource/Page/Hello/Sekai.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package    Sandbox
+ * @subpackage Resource
+ */
+namespace Sandbox\Resource\Page\Hello;
+
+use BEAR\Sunday\Resource\AbstractPage as Page;
+
+/**
+ * Hello World page
+ *
+ * @package    Sandbox
+ * @subpackage Resource
+ */
+class World extends Page
+{
+    public $body = [
+        'greeting' => ''
+    ];
+    
+    /**
+     * @return World
+     */
+    public function onGet($name = "BEAR")
+    {
+        $this['greeting'] = "Hello " . $name;
+        return $this;
+    }
+}
