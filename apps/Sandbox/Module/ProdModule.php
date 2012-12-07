9c47180eb25686b97ec767a9de0333a0fd025f46
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-12-08 00:24:42 +0900

    pass tests

--- apps/Sandbox/Module/ProdModule.php
+++ apps/Sandbox/Module/ProdModule.php
@@ -6,7 +6,8 @@
 namespace Sandbox\Module;
 
 use BEAR\Sunday\Module as SundayModule;
-use BEAR\Package\Module\Package as PackageModule;
+use BEAR\Package\Module as PackageModule;
+
 use Ray\Di\AbstractModule;
 
 /**
@@ -25,7 +26,7 @@ class ProdModule extends AbstractModule
     {
         $config = include __DIR__ . '/config.php';
         // dependency binding (DI)
-        $this->install(new Common\AppModule($config, $this));
+        $this->install(new Common\AppModule($config));
         // aspect weaving (AOP)
         $this->install(new PackageModule\Package\AspectModule($this));
     }
--- apps/Sandbox/Module/TestModule.php
+++ apps/Sandbox/Module/TestModule.php
@@ -8,7 +8,7 @@ namespace Sandbox\Module;
 use Sandbox\Module\ProdModule;
 use BEAR\Sunday\Module;
 use BEAR\Sunday\Module\Constant;
-use BEAR\Package\Module\Resource;
+use BEAR\Package\Module as PackageModule;
 
 /**
  * Production module
@@ -27,7 +27,10 @@ class TestModule extends ProdModule
         $config = require __DIR__ . '/config.php';
         $config['master_db']['dbname'] = 'blogbeartest';
         $config['slave_db'] = $config['master_db'];
+        // dependency binding (DI)
         $this->install(new Common\AppModule($config));
-        $this->install(new Resource\NullCacheModule($this));
+        $this->install(new PackageModule\Resource\NullCacheModule($this));
+        // aspect weaving (AOP)
+        $this->install(new PackageModule\Package\AspectModule($this));
     }
 }
