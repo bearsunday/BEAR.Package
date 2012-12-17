3c2a919c93de33daad2fce56985fb37f35124492
Akihito Koriyama <akihito.koriyama@gmail.com>
2012-12-17 18:27:20 +0900

    add e() for instant exception for debug

---
+++ src/BEAR/Package/Debug/Exception/Debug.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @package BEAR.Package
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Debug\Exception;
+
+/**
+ * Debug exception
+ *
+ * @package   BEAR.Package
+ * @subpackge Exception
+ */
+class Debug extends \RuntimeException
+{
+}
