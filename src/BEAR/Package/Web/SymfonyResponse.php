1e71e0ebfc1ff5ac0e87150317a45d85dd6c2ad5
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-01-23 03:48:06 +0900

    rm gomi

--- src/BEAR/Package/Web/SymfonyResponse.php
+++ src/BEAR/Package/Web/SymfonyResponse.php
@@ -15,7 +15,6 @@ use BEAR\Sunday\Web\ResponseInterface;
 use BEAR\Package\Output\Console;
 use BEAR\Resource\Logger;
 use BEAR\Resource\ObjectInterface as ResourceObject;
-use BEAR\Resource\Abject;
 
 use BEAR\Sunday\Resource\AbstractPage as Page;
 use Symfony\Component\HttpFoundation\Response;
