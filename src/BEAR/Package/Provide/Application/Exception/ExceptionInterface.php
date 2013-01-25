0311d26537151ce2a5d598b4f807c9256d65b995
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-01-25 18:09:08 +0900

    move to Package

--- src/BEAR/Package/Provide/Application/Exception/ExceptionInterface.php
+++ src/BEAR/Package/Provide/Application/Exception/ExceptionInterface.php
@@ -5,7 +5,7 @@
  * @package BEAR.Package
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
-namespace BEAR\Package\Exception;
+namespace BEAR\Package\Provide\Application\Exception;
 
 /**
  * Generic exception interface
