19ab324bb309bfae61c9e7f81e617a513e3473b7
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-01-25 14:48:58 +0900

    BEAR\Package\Distribution to BEAR\Installer

--- src/BEAR/Installer/Composer.php
+++ src/BEAR/Installer/Composer.php
@@ -5,7 +5,7 @@
  * @package BEAR.Package
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
-namespace BEAR\Package\Distribution;
+namespace BEAR\Installer;
 
 use Composer\Script\Event;
 use RecursiveIteratorIterator;
@@ -16,7 +16,7 @@ use RecursiveDirectoryIterator;
  *
  * @package    BEAR.Package
  */
-class Installer
+class Composer
 {
     /**
      * @param Event $event
