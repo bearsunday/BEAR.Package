d1355779e832f7ad2ca4c67998429fd38b630588
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-01-31 16:01:25 +0900

    add DI inject log

---
+++ src/BEAR/Package/Provide/Application/DiLogger.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @package BEAR.Package
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Application;
+
+use Ray\Aop\Bind;
+use Ray\Di\LoggerInterface;
+
+/**
+ * Di logger
+ */
+class DiLogger implements LoggerInterface
+{
+    /**
+     * @var string
+     */
+    public $logMessage = '';
+
+    public function log($class, array $params, array $setter, $object, Bind $bind)
+    {
+        $toStr = function ($params) {
+            foreach ($params as &$param) {
+                if (is_object($param)) {
+                    $param = '(' . get_class($param) . ')';
+                } elseif (is_scalar($param)) {
+                    $param = '(' . gettype($param) . ') ' . $param;
+                } elseif (is_callable($param)) {
+                    $param = '(Callable)';
+                }
+            }
+            return implode(', ', $params);
+        };
+        $constructor = $toStr($params);
+        $constructor = $constructor ? $constructor : '';
+        $setter = $setter ? "setter[" . implode(', ', array_keys($setter)) . ']': '';
+        $logMessage = "[DI] {$class} construct[$constructor] {$setter}";
+        $this->logMessage = $logMessage;
+    }
+}
