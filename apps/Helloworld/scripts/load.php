7abca8cdb6e19d8f969af5bd26604b0c865bd068
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-01 21:52:24 +0900

    fix app tests

--- apps/Helloworld/scripts/load.php
+++ apps/Helloworld/scripts/load.php
@@ -9,12 +9,7 @@ $packageDir = dirname(dirname(dirname(__DIR__)));
 // Auto loader
 require_once $packageDir . '/vendor/autoload.php';
 
-
 // Annotation auto loader
-AnnotationRegistry::registerAutoloadNamespace(__NAMESPACE__ . '\Annotation\\', dirname(__DIR__));
-AnnotationRegistry::registerAutoloadNamespace('Ray\Di\Di\\', $packageDir . '/vendor/ray/di/src/');
-AnnotationRegistry::registerAutoloadNamespace('BEAR\Resource\Annotation\\', $packageDir . '/vendor/bear/resource/src/');
-AnnotationRegistry::registerAutoloadNamespace('BEAR\Sunday\Annotation\\', $packageDir . '/vendor/bear/sunday/src/');
-AnnotationReader::addGlobalIgnoredName('noinspection');
-AnnotationReader::addGlobalIgnoredName('returns'); // for Mr.Smarty. :(
+$loader = require $packageDir . '/vendor/autoload.php';
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 unset($packageDir);
