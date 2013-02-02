f45059a8cf55fc42f9ddf0fb57766d573c579a1c
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-02 16:35:20 +0900

    [#7] resource browser

---
+++ apps/Sandbox/public/_dev/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dev tools</title>
+    <!-- Bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/css/bootstrap-combined.min.css" rel="stylesheet">
+</head>
+<body>
+<ul>
+    <h2>Dev Tools</h2>
+
+    <div class="awell">
+        <ul>
+            <li><a href="ide/index.php"><i class="icon-th-list"></i> IDE</a></li>
+            <li><a href="memcache.php"><i class="icon-zoom-in"></i> Memcache admin</a></li>
+            <li><a href="apc.php"><i class="icon-zoom-in"></i> APC admin</a></li>
+            <li><a href="apc.php?SCOPE=A&SORT1=H&SORT2=D&COUNT=20&OB=3&object_only"><i class="icon-zoom-in"></i> APC Objects</a></li>
+            <li><a href="phpinfo.php"><i class="icon-info-sign"></i> phpinfo();</a></li>
+            <li><a href="data/log/di.log.txt"><i class="icon-info-sign"></i> Module (DI/AOP)</a></li>
+            <li><a href="refresh.php"><i class="icon-refresh"></i> Refresh (cache clear)</a></li>
+        </ul>
+    </div>
+</ul>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ src/BEAR/Package/Dev/Application/ApplicationReflector.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @package BEAR.Package
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Dev\Application;
+
+use BEAR\Sunday\Extension\Application\AppInterface;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
+use BEAR\Resource\AbstractObject as ResourceOjbect;
+
+/**
+ * Application reflector
+ */
+class ApplicationReflector
+{
+    /**
+     * @var AppInterface
+     */
+    public $app;
+
+    /**
+     * @var string
+     */
+    public $appName;
+
+    /**
+     * @var string
+     */
+    public $appDir;
+
+    /**
+     * @param AppInterface $app
+     */
+    public function __construct(AppInterface $app)
+    {
+        $this->app = $app;
+        $ref = new \ReflectionClass($app);
+        $this->appName = $ref->getNamespaceName();
+        $this->appDir = dirname($ref->getFileName());
+    }
+
+    /**
+     * @return array
+     */
+    public function getResources()
+    {
+        $resources = $this->getResourcesUris();
+        $list = [];
+        foreach ($resources as $uri) {
+            try {
+                $response = $this->app->resource->options->uri($uri)->eager->request();
+                $list[$uri] = [
+                    'class' => get_class($response),
+                    'options' => $response->headers,
+                    'links' => $response->links
+                ];
+            } catch (\BEAR\Resource\Exception\ResourceNotFound $e) {
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param $uri
+     *
+     * @return array
+     * @throws \RuntimeException
+     */
+    public function getNewResource($uri)
+    {
+        $url = parse_url($uri);
+        if (!(isset($url['scheme']) && isset($url['host']))) {
+            throw new \RuntimeException("Invalid URI [$uri]", 400);
+        }
+        $path = implode('/', array_map('ucwords', explode('/', $url['path'])));
+        $filePath = $this->appDir . '/' . 'Resource/' . ucwords($url['scheme']) . $path . '.php';
+        $fileContents = file_get_contents(__DIR__ . '/resource.tpl');
+        $fileContents = str_replace('{$app}', $this->appName, $fileContents);
+        $array = explode('/', $this->appName . $path);
+        $class = array_pop($array);
+        $namespace = implode('\\', $array);
+        $fileContents = str_replace('{$namespace}', $namespace, $fileContents);
+        $fileContents = str_replace('{$class}', $class, $fileContents);
+
+//        $this->filePutContents($filePath, $fileContents);
+
+        return [$filePath, $fileContents];
+
+    }
+
+    public function filePutContents($path, $contents)
+    {
+        $parts = explode('/', $path);
+        $file = array_pop($parts);
+        $dir = '';
+        foreach ($parts as $part) {
+            if (!is_dir($dir .= "/$part")) {
+                var_dump($dir);
+                mkdir($dir);
+                if (!is_writable($dir)) {
+                    throw new \RuntimeException("Not writable dir [$dir]", 500);
+                }
+            }
+        }
+        $file = "$dir/$file";
+        if (file_exists($file)) {
+            throw new \RuntimeException("File already exits file", 500);
+        }
+        file_put_contents($file, $contents);
+        if (!is_writable($file)) {
+            throw new \RuntimeException("Not writable file [$file]", 500);
+        }
+    }
+
+    public function getResourceOptions(ResourceOjbect $ro)
+    {
+        $ref = new \ReflectionClass($ro);
+        $methods = $ref->getMethods();
+        $allow = [];
+        foreach ($methods as $method) {
+            $isRequestMethod = (substr($method->name, 0, 2) === 'on') && (substr($method->name, 0, 6) !== 'onLink');
+            if ($isRequestMethod) {
+                $allow[] = strtolower(substr($method->name, 2));
+            }
+        }
+        $params = [];
+        foreach ($allow as $method) {
+            $refMethod = new \ReflectionMethod($ro, 'on' . $method);
+            $parameters = $refMethod->getParameters();
+            $paramArray = [];
+            foreach ($parameters as $parameter) {
+                $name = $parameter->getName();
+                $param = $parameter->isOptional() ? "({$name})" : $name;
+                $paramArray[] = $param;
+            }
+            $key = "param-{$method}";
+            $params[$key] = implode(',', $paramArray);
+        }
+        $result = ['allow' => $allow, 'params' => $params];
+
+        return $result;
+    }
+
+    /**
+     * @return array resource uri list
+     */
+    private function getResourcesUris()
+    {
+        $resources = [];
+        $resourceDir = $this->appDir . '/Resource';
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($resourceDir), RecursiveIteratorIterator::SELF_FIRST);
+        foreach ($iterator as $item) {
+            /** @var $item \SplFileInfo */
+            if ($item->isFile() && substr($item->getFilename(), -3) === 'php') {
+                $uri = $this->getUri($item, $resourceDir);
+                if ($uri) {
+                    $resources[] = $uri;
+                }
+            }
+        }
+
+        return $resources;
+    }
+
+    /**
+     * @param \SplFileInfo $file
+     * @param string       $resourceDir
+     *
+     * @return string
+     */
+    private function getUri(\SplFileInfo $file, $resourceDir)
+    {
+        $relativePath = strtolower(str_replace($resourceDir . '/', '', (string)$file));
+        $path = explode('/', $relativePath);
+        $scheme = array_shift($path);
+        $appName = 'self';
+        $uri = "{$scheme}://{$appName}/" . implode('/', $path);
+        $uri = substr($uri, 0, -4);
+
+        return $uri;
+    }
+}
