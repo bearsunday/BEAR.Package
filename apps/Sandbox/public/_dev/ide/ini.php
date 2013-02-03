15b8b028a1e21671439518c66e83e2058bbaf256
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-03 10:49:44 +0900

    rm _dev web tool from Sandbox

--- apps/Sandbox/public/_dev/ide/ini.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * editor root
- */
-$rootDir = dirname(dirname(dirname(__DIR__)));
-
-return $rootDir;
--- apps/Sandbox/public/_dev/refresh.php
+++
@@ -1,6 +0,0 @@
-<?php
-// clear APC cache
-error_log('app files cleared by ' . __FILE__);
-require dirname(dirname(__DIR__)) . '/scripts/clear.php';
-
-header('Location: /');
