c3663c5c412150db1e0aae17f0b906598556ce9c
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-11 10:26:22 +0900

    for composer autoloader

--- apps/Skeleton/scripts/Install/Composer.php
+++ apps/Skeleton/scripts/Install/Composer.php
@@ -5,7 +5,7 @@
  * @package BEAR.Package
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
-namespace Skeleton\scripts\Install;
+namespace scripts\Install;
 
 use Composer\Script\Event;
 use RecursiveIteratorIterator;
