c01f8b6d6c5890dcd0cbf00ad0a3174c56fb0246
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-13 17:23:49 +0900

    fix refresh link

---
+++ src/BEAR/Package/Provide/Router/MapInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @package BEAR.Package
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Router;
+
+interface MapInterface
+{
+    /**
+     * Gets a route that matches a given path and other server conditions.
+     *
+     * @param string $path The path to match against.
+     *
+     * @param array $server An array copy of $_SERVER.
+     *
+     * @return Route|false Returns a Route object when it finds a match, or
+     * boolean false if there is no match.
+     *
+     */
+    public function match($path, array $server);
+}
