4d796fcdccb3ad5cd20cbd80996441eed1c05ab8
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-13 12:32:58 +0900

    reduce test dependency

--- apps/Sandbox/tests/bootstrap.php
+++ apps/Sandbox/tests/bootstrap.php
@@ -1,10 +1,6 @@
 <?php
-// application root
-chdir(dirname(__DIR__));
-
 // init
 require_once dirname(__DIR__) . '/scripts/bootstrap.php';
-require dirname(__DIR__) . '/scripts/clear.php';
 
 // set the application path into the globals so we can access
 // it in the tests.
