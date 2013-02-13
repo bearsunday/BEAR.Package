cad65e284d7b4143ad2fb7bff931ec9382ea17f2
Richard McIntyre <richard.mackstar@gmail.com>
2013-02-13 21:54:14 +0000

    Adding Symfony Router Functionality

---
+++ src/BEAR/Package/Provide/Router/SymfonyRouterModule.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the BEAR.Packages package
+ *
+ * @package BEAR.Package
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Router;
+
+use Ray\Di\AbstractModule;
+
+
+/**
+ * Router module
+ *
+ * @package    BEAR.Sunday
+ * @subpackage Module
+ */
+class SymfonyRouterModule extends AbstractModule
+{
+    /**
+     * (non-PHPdoc)
+     * @see Ray\Di.AbstractModule::configure()
+     */
+    protected function configure()
+    {
+        $this
+            ->bind('BEAR\Sunday\Extension\Router\RouterInterface')
+            ->to(__NAMESPACE__ . '\SymfonyRouter');
+    }
+}
