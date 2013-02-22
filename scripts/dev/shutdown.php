774988a9c59b5885cf5186b4428ecbd083219e49
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-23 00:41:10 +0900

    remove dev/shutdown script

--- scripts/dev/shutdown.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @global $mode
- */
-return function () use ($mode){
-    //echo '<a href="/dev/"><button class="btn btn-mini" style="position:absolute;top:8px;right:8px;z-index:9999"><span class=" icon-leaf"></span> ' . $mode . '</button></a>';
-};
