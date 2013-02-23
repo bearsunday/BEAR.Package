0393c2dc3060e1cdc4757c411561661719d4f3fe
akihito.koriyama@gmail.com <akihito.koriyama@gmail.com>
2013-02-23 12:45:10 +0900

    fix systax error

--- src/BEAR/Package/Module/Stub/Stub.php
+++ src/BEAR/Package/Module/Stub/Stub.php
@@ -5,7 +5,7 @@
  * @package BEAR.Package
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
-namespace BEAR\Package\Module\Stub
+namespace BEAR\Package\Module\Stub;
 
 use BEAR\Sunday\Inject\LogInject;
 use Ray\Aop\MethodInterceptor;
