70eef8a51f89c54a4aa1071248178cf77f7ff581
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-02-28 16:50:55 +0900

    [#22] fix IDE project path

--- docs/dev/public/ide/init.js.php
+++ docs/dev/public/ide/init.js.php
@@ -5,12 +5,11 @@ use BEAR\Package\Dev\DevWeb\Editor\FileTree;
 $projectDir = '';
 
 $root = require __DIR__ . '/ini.php';
-//$root = $_GET['root'];
 $tree = new FileTree($root);
 //$tree->tree('#container_id1', $files['page'], '<span class=\"tree_label\">Page</span>');
 //$tree->tree('#container_id2', $files['ro'], '<span class=\"tree_label\">Resource</span>');
 //$tree->tree('#container_id3', $files['view'], '<span class=\"tree_label\">View template</span>');
-$tree->tree('#container_id', $projectDir , '<span class=\"tree_label\">Project</span>');
-$initialOpeningFile ='App.php';
+$tree->tree('#container_id', '/' , '<span class=\"tree_label\">Project</span>');
+$initialOpeningFile =$root . '/App.php';
 $js = $tree->getJsCode($initialOpeningFile);
 echo $js;
