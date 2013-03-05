20e5f448ce07de47e6b8ff3efa1db03f0d48c42f
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-03-05 10:45:09 +0900

    fix composer install path

--- bin/new_app.php
+++ bin/new_app.php
@@ -23,10 +23,12 @@ if (! isset($argv[1])) {
     throw new \LogicException('new application name required. usage: new_app.php MyApp');
 }
 
-if (! file_exists('../../composer.phar')) {
-    $composerCmd = 'cd ../..; curl -s https://getcomposer.org/installer | php';
+$composerPath = dirname(__DIR__) . '/composer.phar';
+if (! file_exists($composerPath)) {
+    $composerCmd = 'curl -s https://getcomposer.org/installer | php';
     passthru($composerCmd);
+    $composerPath = dirname(__DIR__) . '/composer.phar';
 }
 $dir = dirname(__DIR__) . '/apps/';
-$cmd = "php ../../composer.phar create-project -s dev --dev bear/skeleton {$dir}/{$argv[1]}";
+$cmd = "php {$composerPath} create-project -s dev --dev bear/skeleton {$dir}/{$argv[1]}";
 passthru($cmd);
