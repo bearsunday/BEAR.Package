d62ead2255ef670f381e972ff3efd1ab0c08adba
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-03-12 20:52:23 +0900

    bin/new_res.php

---
+++ bin/new_res.php
@@ -0,0 +1,35 @@
+<?php
+
+use BEAR\Package\Dev\Application\ApplicationReflector;
+
+/**
+ * New resource
+ *
+ * usage:
+ *
+ * $ php bin/new_res.php MyApp page://self/hello/world
+ *
+ */
+$appName = isset($argv[1]) ? $argv[1] : error();
+$uri = isset($argv[2]) ? $argv[2] : error();
+$appFile = "apps/{$appName}/scripts/instance.php";
+if (! file_exists($appFile)) {
+   error("Invalid application name: {$appName}");
+}
+$app = require $appFile;
+$appReflector = new ApplicationReflector($app);
+try {
+    list($result, $filePath) = $appReflector->newResource($uri);
+} catch (\Exception $e) {
+    error($e->getMessage());
+}
+echo "new resource [$uri] was successfully made at [{$filePath}]" . PHP_EOL;
+
+/**
+ * @param string $msg
+ */
+function error($msg = 'Usage: php bin/new_res.php <app-name> <new-uri>')
+{
+    echo $msg . PHP_EOL;
+    exit(1);
+}
