683b2fb13851acf54d2bb89934fd427c80b56043
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-04-02 02:20:05 +0900

    move DevWeb to Web

--- src/BEAR/Package/Dev/Web/Editor/FileTree.php
+++ src/BEAR/Package/Dev/Web/Editor/FileTree.php
@@ -5,7 +5,7 @@
  * @package BEAR.Package
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
-namespace BEAR\Package\Dev\DevWeb\Editor;
+namespace BEAR\Package\Dev\Web\Editor;
 
 /**
  * File tree
