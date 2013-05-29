2379465d6e5e01e7db5512e6138b1c3a9e3026f5
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-05-29 11:34:47 +0900

    add bear/ace editor link in error

--- apps/Sandbox/scripts/develop/instance.php
+++ apps/Sandbox/scripts/develop/instance.php
@@ -12,6 +12,7 @@ ini_set('display_errors', 1);
 ini_set('xhprof.output_dir', sys_get_temp_dir());
 ini_set('xdebug.collect_params', 0);
 ini_set('xdebug.var_display_max_depth', 1);
+ini_set('xdebug.file_link_format', '/dev/edit/?file=%f&line=$l');
 
 require dirname(__DIR__) . '/bootstrap.php';
 
