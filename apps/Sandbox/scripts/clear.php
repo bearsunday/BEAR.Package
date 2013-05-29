2e922bd13e84aaba9854598becc9bb0be61cfa13
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-05-29 11:36:30 +0900

    apc 4.0 incompatibility

--- apps/Sandbox/scripts/clear.php
+++ apps/Sandbox/scripts/clear.php
@@ -11,7 +11,9 @@
 
 // APC Cache
 if (function_exists('apc_clear_cache')) {
-    apc_clear_cache('user');
+    if (version_compare(phpversion('apc'), '4.0.0') < 0) {
+        apc_clear_cache('user');
+    }
     apc_clear_cache();
 }
 
