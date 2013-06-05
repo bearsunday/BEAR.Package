bfd93e1627b7ad09afce5d9ea408c5c3c2c52e1a
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-06-05 13:38:23 +0900

    add skeleton version

--- bin/new_app.php
+++ bin/new_app.php
@@ -18,6 +18,8 @@
  * php web.php get /
  * php -S localhost:8088 web.php
  */
+
+$skeletonVersion = "0.7.2";
 $appName = isset($argv[1]) ? ucwords($argv[1]) : 'NewApp';
 
 $composerPath = dirname(__DIR__) . '/composer.phar';
@@ -27,5 +29,5 @@ if (!file_exists($composerPath)) {
     $composerPath = dirname(__DIR__) . '/composer.phar';
 }
 $dir = dirname(__DIR__) . '/apps';
-$cmd = "php {$composerPath} create-project --dev bear/skeleton {$dir}/$appName";
+$cmd = "php {$composerPath} create-project --dev bear/skeleton {$dir}/{$appName} {$skeletonVersion}";
 passthru($cmd);
