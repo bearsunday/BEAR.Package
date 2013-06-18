397801d8b83cbe378a704828b19169000bf0d223
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-06-18 11:57:59 +0900

    refactor Ded and dev/instance

--- apps/Sandbox/scripts/develop/instance.php
+++ apps/Sandbox/scripts/develop/instance.php
@@ -3,46 +3,24 @@
  * Return application instance with development setup
  *
  * @global $mode
+ *
+ * @return BEAR\Sunday\Extension\Application\AppInterface
  */
 
 use BEAR\Package\Dev\Dev;
-use BEAR\Package\Dev\Resource\ResourceLog;
 
-require dirname(__DIR__) . '/bootstrap.php';
-require $packageDir . '/scripts/develop/ini.php';
+// set application root as current directory
+chdir(dirname(dirname(__DIR__)));
 
-$logDir = dirname(dirname(__DIR__)) . '/data/log';
+require 'scripts/bootstrap.php';
 
-// dev tools: fatal error / syntax error / exception handler
-$dev = new Dev;
-$dev->loadDevFunctions()
+// development configuration
+$app = (new Dev)
+    ->iniSet()
+    ->loadDevFunctions()
     ->registerFatalErrorHandler()
-    ->registerExceptionHandler($logDir)
-    ->registerSyntaxErrorEdit();
-
-// direct file for built in web server
-if ($dev->directAccessFile() === false) {
-    return false;
-}
-
-
-// console args
-$mode = isset($argv[3]) ? $argv[3] : $mode;
-$app = require dirname(__DIR__) . '/instance.php';
-/** @var $app \BEAR\Package\Provide\Application\AbstractApp */
-
-// Use cli parameter for routing (web.php get /)
-if (PHP_SAPI === 'cli') {
-    $app->router->setArgv($argv);
-} else {
-    $app->router->setGlobals($GLOBALS);
-    $argv = [];
-}
-
-// development web service (/dev)
-$dev->setApp($app)->webService();
-
-// resource log
-$app->logger->register($app);
+    ->registerExceptionHandler('/data/log')
+    ->registerSyntaxErrorEdit()
+    ->getDevApplication($mode);
 
 return $app;
