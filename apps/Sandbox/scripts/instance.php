9059a2c4f490e9b9044f8998c8c5bcb3cd6722cc
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-06-19 10:53:27 +0900

    use simple closure to create application instance
    remove ApplicationFactory

--- apps/Sandbox/scripts/instance.php
+++ apps/Sandbox/scripts/instance.php
@@ -7,14 +7,31 @@
  */
 namespace Sandbox;
 
-use BEAR\Package\Provide\Application\ApplicationFactory;
+use BEAR\Package\Provide\Application\DiLogger;
+use Doctrine\Common\Cache\ApcCache;
+use Doctrine\Common\Cache\FilesystemCache;
+use Ray\Di\Injector;
 
 require_once __DIR__ . '/bootstrap.php';
 
 // mode
 $mode = isset($mode) ? $mode : 'Prod';
 
+$cache = function_exists('apc_fetch') ? new ApcCache : new FilesystemCache(dirname(__DIR__) . '/data/tmp/cache');
+$appKey = __NAMESPACE__ . $mode;
+
+// return cached application
+if ($cache->contains($appKey)) {
+    return $cache->fetch($appKey);
+}
+
 // new application instance
-$app = (new ApplicationFactory)->newInstance(__NAMESPACE__, $mode);
+$diLogger = new DiLogger;
+$injector = Injector::create([new Module\AppModule($mode)], $cache)->setLogger($diLogger);
+$app = $injector->getInstance('\BEAR\Sunday\Extension\Application\AppInterface');
+$cache->save($appKey, $app);
+
+// di log
+file_put_contents(dirname(__DIR__) . '/data/log/di.log', (string)$injector . (string)$diLogger);
 
 return $app;
