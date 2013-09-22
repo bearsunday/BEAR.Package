493ac159ac164e9519e9cb03dacc25fa1f47257f
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-09-22 18:07:16 +0900

    set initial  dev/ide status to nothing opened

--- var/www/dev/ide/init.js.php
+++ var/www/dev/ide/init.js.php
@@ -10,7 +10,7 @@ $tree = new FileTree($root);
 //$tree->tree('#container_id2', $files['ro'], '<span class=\"tree_label\">Resource</span>');
 //$tree->tree('#container_id3', $files['view'], '<span class=\"tree_label\">View template</span>');
 $tree->tree('#container_id', '/' , '<span class=\"tree_label\">Project</span>');
-$initialOpeningFile =$root . '/App.php';
+$initialOpeningFile =$root . '/';
 $js = $tree->getJsCode($initialOpeningFile);
 
 echo $js;
