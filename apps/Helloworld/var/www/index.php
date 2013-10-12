f583068557e15457882ae6f24e32c9072ce4cdf7
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-10-13 02:40:33 +0900

    rename basic.php index.php

--- apps/Helloworld/var/www/index.php
+++ apps/Helloworld/var/www/index.php
@@ -22,3 +22,7 @@ foreach ($response->headers as $header) {
     header($header);
 }
 echo $response->body . PHP_EOL;
+
+
+
+$app = Injector::create([new ApplicationBinding])->getInstance('AppInterface');
