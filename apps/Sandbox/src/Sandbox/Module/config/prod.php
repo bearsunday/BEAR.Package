13b86d61b5d354b9c8c5c6b6a61d8b1c7021bade
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-10-15 01:01:58 +0900

    mysql to sqlite

--- apps/Sandbox/src/Sandbox/Module/config/prod.php
+++ apps/Sandbox/src/Sandbox/Module/config/prod.php
@@ -10,28 +10,44 @@ $slavePassword = isset($_SERVER['BEAR_DB_PASSWORD_SLAVE']) ? $_SERVER['BEAR_DB_P
 
 $appDir = dirname(dirname(dirname(dirname(__DIR__))));
 // @Named($key) => instance
+
+$sqlite = [
+    'driver' => 'pdo_sqlite',
+    'path' =>  $appDir . '/var/db/posts.sq3'
+];
+
 $config = [
     // database
-    'master_db' => [
-        'driver' => 'pdo_mysql',
-        'host' => 'localhost',
-        'dbname' => 'blogbear',
-        'user' => $id,
-        'password' => $password,
-        'charset' => 'UTF8'
-    ],
-    'slave_db' => [
-        'driver' => 'pdo_mysql',
-        'host' => 'localhost',
-        'dbname' => 'blogbear',
-        'user' => $slaveId,
-        'password' => $slavePassword,
-        'charset' => 'UTF8'
-    ],
+    'master_db' => $sqlite,
+    'slave_db' => $sqlite,
     // constants
     'tmp_dir' => $appDir . '/var/tmp',        // set faster storage if possible
     'log_dir' => $appDir . '/var/log',
     'vendor_dir' => $appDir . '/var/lib',
 ];
+
+//$config = [
+//    // database
+//    'master_db' => [
+//        'driver' => 'pdo_mysql',
+//        'host' => 'localhost',
+//        'dbname' => 'blogbear',
+//        'user' => $id,
+//        'password' => $password,
+//        'charset' => 'UTF8'
+//    ],
+//    'slave_db' => [
+//        'driver' => 'pdo_mysql',
+//        'host' => 'localhost',
+//        'dbname' => 'blogbear',
+//        'user' => $slaveId,
+//        'password' => $slavePassword,
+//        'charset' => 'UTF8'
+//    ],
+//    // constants
+//    'tmp_dir' => $appDir . '/var/tmp',        // set faster storage if possible
+//    'log_dir' => $appDir . '/var/log',
+//    'vendor_dir' => $appDir . '/var/lib',
+//];
 
 return $config;
