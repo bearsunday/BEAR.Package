51c3b99833dff0877d713bcb5f28878573334beb
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-10-22 02:16:04 +0900

    use CDN for /dev/js and css

--- var/www/dev/view/layout.php
+++ var/www/dev/view/layout.php
@@ -8,15 +8,14 @@ return <<<EOT
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="../../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
         }
     </style>
-    <link href="../../assets/css/bootstrap-responsive.css" rel="stylesheet">
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+    <link rel="shortcut icon" href="/favicon.png">
 </head>
 
 <body>
@@ -34,20 +33,8 @@ return <<<EOT
 <!-- Le javascript
     ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="../../assets/js/jquery.js"></script>
-<script src="../../assets/js/bootstrap-transition.js"></script>
-<script src="../../assets/js/bootstrap-alert.js"></script>
-<script src="../../assets/js/bootstrap-modal.js"></script>
-<script src="../../assets/js/bootstrap-dropdown.js"></script>
-<script src="../../assets/js/bootstrap-scrollspy.js"></script>
-<script src="../../assets/js/bootstrap-tab.js"></script>
-<script src="../../assets/js/bootstrap-tooltip.js"></script>
-<script src="../../assets/js/bootstrap-popover.js"></script>
-<script src="../../assets/js/bootstrap-button.js"></script>
-<script src="../../assets/js/bootstrap-collapse.js"></script>
-<script src="../../assets/js/bootstrap-carousel.js"></script>
-<script src="../../assets/js/bootstrap-typeahead.js"></script>
-
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 </body>
 </html>
 EOT;
