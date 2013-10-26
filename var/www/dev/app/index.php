eb958badbc3872a8554aad4ee199883b06a0de6d
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-10-26 16:54:37 +0900

    option to see all dependency graph option

--- var/www/dev/app/index.php
+++ var/www/dev/app/index.php
@@ -1,9 +1,15 @@
 <?php
 
+use Printo\Printo;
+
 if (isset($_GET['text'])) {
     echo serialize($app);
 } elseif (isset($_GET['print_r'])) {
     echo print_r($app);
 } else {
-    print_o($app);
+    $progress = (!isset($_GET['all']));
+    Printo::init(
+        ['showProgressive' => $progress]
+    );
+    echo (new Printo($app));
 }
