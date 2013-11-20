583da433f62a7690219b8cf4d6bf16a4ac980809
MugeSo <mugeso@mugeso.com>
2013-11-20 19:43:49 +0000

    Add link to jquery.js in devtool layout

--- var/www/dev/view/layout.php
+++ var/www/dev/view/layout.php
@@ -33,6 +33,7 @@ return <<<EOT
 <!-- Le javascript
     ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 </body>
