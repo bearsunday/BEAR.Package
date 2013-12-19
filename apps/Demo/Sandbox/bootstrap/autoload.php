7f3d764a6ffd0cac95724d50225e16f7d4193931
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-12-20 02:02:30 +0900

    [refactor] instace / autoload script

--- apps/Demo/Sandbox/bootstrap/autoload.php
+++ apps/Demo/Sandbox/bootstrap/autoload.php
@@ -1,24 +1,21 @@
 <?php
+
+namespace Demo\Sandbox;
+
 /**
- * load script
+ * Autoloader
  *
- *  set composer auto loader
- *  set silent auto loader for doctrine annotation
- *  set ignore annotation
- *
- * @global $PackageDir
+ * @return $app \Composer\Autoload\ClassLoader
  */
-
-use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
-
-umask(0);
 
 $packageDir = dirname(dirname(dirname(dirname(__DIR__))));
 $loader = require $packageDir . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
-$loader->set('Demo\Sandbox', dirname(__DIR__) . '/src');
 
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-AnnotationReader::addGlobalIgnoredName('noinspection'); // for phpStorm
-AnnotationReader::addGlobalIgnoredName('returns'); // for Mr.Smarty. :(
+\BEAR\Bootstrap\registerLoader(
+    $loader,
+    $packageDir,
+    __NAMESPACE__,
+    dirname(__DIR__)
+);
