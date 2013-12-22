49e82b93851b8bc06d5ab17ca97aefc7fc05b4e2
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-12-22 12:42:33 +0900

    fix modal.js

--- apps/Demo/Sandbox/var/www/assets/js/modal.js
+++ apps/Demo/Sandbox/var/www/assets/js/modal.js
@@ -1,28 +1,7 @@
 var MyDialogs = {
-    loadConfirmationModal:function (modalId, confirmURL, caption, body) {
-        var $modal = jQuery('#' + modalId);
-        if ($modal.size() === 0) {
-            var modalString = '<div id="'
-                + modalId
-                + '" class="modal fade" role="dialog" tabindex="-1" aria-hidden="true">'
-                + '<div class="modal-dialog"><div class="modal-content">'
-                + '<div class="modal-header">'
-                + '<button class="close" data-dismiss="modal" aria-hidden="true">&times;</button>'
-                + '<h3 class="modal-title">'
-                + caption
-                + '</h3>'
-                + '</div>'
-                + '<div class="modal-body">'
-                + body
-                + '</div>'
-                + '<div class="modal-footer">'
-                + '<button id="cancel" class="btn" data-dismiss="modal" type="button" name="cancel">Cancel</button>'
-                + '<a id="submit" class="btn btn-danger" href="'
-                + confirmURL + '">Delete</a>' + '</div></div></div></div>';
-            console.log(modalString);
-            $modal = jQuery(modalString);
+    confirm:function (confirmURL, body) {
+        if(window.confirm(body)){
+            location.href = confirmURL;
         }
-        $modal.modal('show');
-        return false;
     }
-};
+}
