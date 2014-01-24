b45b3e440eca6d5cb9fdc8beec396760c8cf37a4
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-24 19:29:41 +0900

    remove xdebug.var_display_max_depth set

--- src/Dev/Dev.php
+++ src/Dev/Dev.php
@@ -143,7 +143,6 @@ class Dev
         ini_set('xhprof.output_dir', sys_get_temp_dir());
         ini_set('xdebug.collect_params', 0);
         ini_set('xdebug.max_nesting_level', 500);
-        ini_set('xdebug.var_display_max_depth', 1);
         ini_set('xdebug.file_link_format', '/dev/edit/?file=%f&line=$l');
 
         return $this;
