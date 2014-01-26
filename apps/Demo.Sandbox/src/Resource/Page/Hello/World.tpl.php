aafa4d4429cd5e8b2537b799f6dfc84ed7f23e0b
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-26 22:21:47 +0900

    template for smarty, twig and aura/view

--- apps/Demo.Sandbox/src/Resource/Page/Hello/World.tpl.php
+++ apps/Demo.Sandbox/src/Resource/Page/Hello/World.tpl.php
@@ -1,17 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Hello World</title>
-    <style>
-        body {
-            padding: 20px;
-        }
-    </style>
-</head>
-
 <body>
     <h1><?= $this->greeting;?></h1>
-<p>powered by Aura.View</p>
+    <p>by Aura.View</p>
 </body>
 </html>
