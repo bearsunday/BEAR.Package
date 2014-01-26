914601c4243e30a7d0338456e37d93cdb88c57ce
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-26 21:16:55 +0900

    add clearApp script

--- apps/Demo.Sandbox/bootstrap/contexts/dev.php
+++ apps/Demo.Sandbox/bootstrap/contexts/dev.php
@@ -23,13 +23,19 @@ ob_start();
 if ((PHP_SAPI !== 'cli') && preg_match('/\.(?:png|jpg|jpeg|gif|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;
 }
-
 $appDir = dirname(dirname(__DIR__));
+
+//
+// The cache is cleared on each request via the following script. We understand that you may want to debug
+// your application with caching turned on. When doing so just comment out the following.
+//
+require $appDir . '/bin/clear.php';
 
 // Here we get an application instance by setting a $context variable such as (prod, dev, api)
 // the dev instance provides debugging tools and defaults to help you the development of your application.
 $context = 'dev';
 $app = require $appDir . '/bootstrap/instance.php';
+
 /* @var $app \BEAR\Package\Provide\Application\AbstractApp */
 $dev = new Dev;
 $dev
@@ -46,12 +52,6 @@ $dev
 if ($dev->isDirectStaticFileAccess()) {
     return false;
 }
-
-//
-// The cache is cleared on each request via the following script. We understand that you may want to debug
-// your application with caching turned on. When doing so just comment out the following.
-//
-require $appDir . '/bin/clear.php';
 
 //
 // Calling the match of a BEAR.Sunday compatible router will give us the $method, $pagePath, $query to be used
