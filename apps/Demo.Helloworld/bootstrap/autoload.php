f57811b2e943eb42fe6876b9c176d46b7d8ee847
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-02-05 23:43:18 +0900

    loader for Demo.Helloworld

--- apps/Demo.Helloworld/bootstrap/autoload.php
+++ apps/Demo.Helloworld/bootstrap/autoload.php
@@ -2,7 +2,17 @@
 
 namespace Demo\Helloworld;
 
-// Annotation auto loader
-$loader = require  dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php';
-/** @var $loader \Composer\Autoload\ClassLoader */
-$loader->addPsr4('Demo\Helloworld\\', dirname(__DIR__) . '/src');
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\Common\Annotations\AnnotationReader;
+
+preload: {
+//    require dirname(__DIR__) . '/var/tmp/preloader/preload.min.php';
+}
+
+autoload: {
+    $loader = require  dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php';
+    /** @var $loader \Composer\Autoload\ClassLoader */
+    $loader->addPsr4('Demo\Helloworld\\', dirname(__DIR__) . '/src');
+    AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+}
+
