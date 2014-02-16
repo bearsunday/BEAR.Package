b5e1225cf7dec3d7a513bdbadcf839e27e08c7bb
atakig <atakigu@gmail.com>
2014-02-16 23:16:14 +0900

    correspond to absolute path and apps/Application Dir and Application Dir

--- bin/server.php
+++ bin/server.php
@@ -2,13 +2,14 @@
 
 // options
 $appDir = isset($argv[$argc - 1]) ? $argv[$argc - 1] : error();
+$appDir = adjustAppDir($appDir);
 $opt = getopt('', [ "context::", "port::", "php::" ]);
 $port = isset($opt['port']) ? $opt['port'] : '8080';
 $context = isset($opt['context']) ? $opt['context'] : 'dev';
 $php = isset($opt['php']) ? $opt['php'] : 'php';
-$router = dirname(__DIR__) . "/{$appDir}/bootstrap/contexts/{$context}.php";
+$router = "{$appDir}/bootstrap/contexts/{$context}.php";
 if (! file_exists($router)) {
-    $router = dirname(__DIR__) . "/{$appDir}/var/www/index.php";
+    $router = "{$appDir}/var/www/index.php";
 }
 
 if (! file_exists("{$appDir}/var/www")) {
@@ -19,7 +20,7 @@ if (! file_exists($router)) {
     error("invalid context:{$context}");
 }
 
-$root =  dirname(__DIR__) . "/{$appDir}/var/www/";
+$root =  "{$appDir}/var/www/";
 $cmd = "{$php} -S 0.0.0.0:{$port} -t {$root} {$router}";
 $spec = [0 => STDIN, 1 => STDOUT, 2 => STDERR];
 
@@ -35,4 +36,25 @@ function error($msg = 'Usage: php bin/server.php [--port=<port>] [--context=<con
 {
     error_log($msg);
     exit(1);
+}
+
+/**
+ * 引数のappDirからフルパスのappDirを作成して返す
+ *
+ * @param $appDir
+ * @return string
+ */
+function adjustAppDir($appDir) {
+    // 絶対パスを指定
+    if (strpos($appDir, '/') === 0 || preg_match('/^[C-Z]:\w+$/',$appDir) === 1) {
+        return $appDir;
+    // appsから指定
+    } elseif (strpos($appDir, 'apps' . DIRECTORY_SEPARATOR) === 0) {
+        return dirname(__DIR__) . DIRECTORY_SEPARATOR . $appDir;
+    // appDirのみを指定
+    } elseif (strpos($appDir, DIRECTORY_SEPARATOR) === false) {
+        return dirname(__DIR__) . DIRECTORY_SEPARATOR . "apps" .DIRECTORY_SEPARATOR . $appDir;
+    } else {
+        error();
+    }
 }
