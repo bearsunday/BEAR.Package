2390defdc3367424bcf34a0aa7bbd11f9f0bbb96
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-03-14 19:58:53 +0900

    add di compiler module

---
+++ src/Module/Di/DiCompilerProvider.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Module\Di;
+
+use Doctrine\Common\Cache\FilesystemCache;
+use Ray\Di\DiCompiler;
+use Ray\Di\ProviderInterface;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Named;
+
+class DiCompilerProvider implements ProviderInterface
+{
+    /**
+     * @var string
+     */
+    private $appName;
+
+    /**
+     * @var string
+     */
+    private $context;
+
+    /**
+     * @var string
+     */
+    private $tmpDir;
+
+    /**
+     * @param string $appName
+     * @param string $context
+     * @param string $tmpDir
+     *
+     * @Inject
+     * @Named("appName=app_name,context=app_context,tmpDir=tmp_dir")
+     */
+    public function __construct($appName, $context, $tmpDir)
+    {
+        $this->appName = $appName;
+        $this->context = $context;
+        $this->tmpDir = $tmpDir;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \Ray\Di\DiCompiler
+     */
+    public function get()
+    {
+        static $compiler;
+
+        if ($compiler) {
+            return $compiler;
+        }
+        $moduleProvider = function() {
+            $appModule = "{$this->appName}\Module\AppModule";
+            return new $appModule($this->context);
+        };
+        $cacheKey = $this->appName . $this->context;
+        $cache = new FilesystemCache($this->tmpDir);
+        $compiler = DiCompiler::create($moduleProvider, $cache, $cacheKey, $this->tmpDir);
+
+        return $compiler;
+    }
+}
