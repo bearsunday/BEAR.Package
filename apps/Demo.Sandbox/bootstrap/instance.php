71ddb7dd9bd8e031562d7e0972db7deaf559673f
koriym <akihito.koriyama@gmail.com>
2014-03-27 13:17:42 +0900

    function to static method for bootstrap function
    This makes autoload easier.

--- apps/Demo.Sandbox/bootstrap/instance.php
+++ apps/Demo.Sandbox/bootstrap/instance.php
@@ -2,19 +2,22 @@
 /**
  * Application instance script
  *
- * @return $app  \BEAR\Sunday\Extension\Application\AppInterface
+ * @return $app \BEAR\Sunday\Extension\Application\AppInterface
  *
- * @global $context string configuration context
+ * @global $context string application context
  */
 namespace Demo\Sandbox;
 
+use BEAR\Package\Bootstrap\Bootstrap;
+
 require_once __DIR__ . '/autoload.php';
 
-$context = isset($context) ? $context : 'prod';
+// Hierarchical profiler @see http://www.php.net/manual/en/book.xhprof.php
+// \BEAR\Package\Dev\Debug\Debug::xhprof();
 
-$app =  \BEAR\Bootstrap\getApp(
+$app =  Bootstrap::getApp(
     __NAMESPACE__,
-    $context,
+    isset($context) ? $context : 'prod',
     dirname(__DIR__) . '/var/tmp'
 );
 
