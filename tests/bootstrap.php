a78093d9a91895b0ab2211a32ffdaad8388b0426
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-01 13:55:16 +0900

    refactor test loader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\Common\Annotations\AnnotationReader;
+//use Doctrine\Common\Annotations\AnnotationRegistry;
+//use Doctrine\Common\Annotations\AnnotationReader;
 
 ini_set('xdebug.max_nesting_level', 300);
 ini_set('display_errors', 1);
@@ -12,16 +12,18 @@ require dirname(__DIR__) . '/apps/Demo.Sandbox/bin/clear.php';
 // loader
 error_reporting(E_ALL ^ E_NOTICE);
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
+/** @var $loader \Composer\Autoload\ClassLoader */
+$loader->addPsr4('BEAR\Package\\', __DIR__ . '/BEAR/Package');
 error_reporting(E_ALL);
 
-/** @var $loader \Composer\Autoload\ClassLoader */
-AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-AnnotationReader::addGlobalIgnoredName('noinspection');
-AnnotationReader::addGlobalIgnoredName('returns');
+///** @var $loader \Composer\Autoload\ClassLoader */
+//AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+//AnnotationReader::addGlobalIgnoredName('noinspection');
+//AnnotationReader::addGlobalIgnoredName('returns');
 
 // load for p($var); e();
 (new \BEAR\Package\Dev\Dev)->loadDevFunctions();
 
-$GLOBALS['_BEAR_TEST_DIR'] = __DIR__;
-$GLOBALS['_BEAR_TMP_DIR'] = __DIR__ . '/tmp';
-$GLOBALS['_BEAR_PACKAGE_DIR'] = dirname(__DIR__);
+$_ENV['TEST_DIR'] = __DIR__;
+$_ENV['TMP_DIR'] = __DIR__ . '/tmp';
+$_ENV['PACKAGE_DIR'] = dirname(__DIR__);
