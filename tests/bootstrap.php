af470ca457e53b1fb9591aae7d72c5de00fd381a
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-01 13:46:25 +0900

    update test loader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,6 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\AnnotationReader;
 
-error_reporting(E_ALL);
-
 ini_set('xdebug.max_nesting_level', 300);
 ini_set('display_errors', 1);
 
@@ -14,17 +12,16 @@ require dirname(__DIR__) . '/apps/Demo.Sandbox/bin/clear.php';
 // loader
 error_reporting(E_ALL ^ E_NOTICE);
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
-/** @var $loader \Composer\Autoload\ClassLoader */
-$loader->addPsr4('Demo\Sandbox\\', dirname(__DIR__) . '/apps/Demo.Sandbox/src');
 error_reporting(E_ALL);
+
 /** @var $loader \Composer\Autoload\ClassLoader */
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 AnnotationReader::addGlobalIgnoredName('noinspection');
 AnnotationReader::addGlobalIgnoredName('returns');
-$loader->add('BEAR\Package', [__DIR__]);
-$loader->add('Demo\Sandbox', dirname(__DIR__) . '/apps/');
 
+// load for p($var); e();
 (new \BEAR\Package\Dev\Dev)->loadDevFunctions();
+
 $GLOBALS['_BEAR_TEST_DIR'] = __DIR__;
 $GLOBALS['_BEAR_TMP_DIR'] = __DIR__ . '/tmp';
 $GLOBALS['_BEAR_PACKAGE_DIR'] = dirname(__DIR__);
