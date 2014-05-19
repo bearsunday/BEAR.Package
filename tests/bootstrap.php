3b46d6d052edba3a55b54f3b64d4ec7d3017c8ea
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-19 11:47:35 +0900

    Revert "remove deprecated loadDevFunctions function"
    This reverts commit 509c65250fc1fa46da2f2d395fad5b9e7901eb4c.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,10 @@ error_reporting(E_ALL ^ E_NOTICE);
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
 $loader->addPsr4('BEAR\Package\\', __DIR__);
-$loader->addPsr4('Vendor\MockApp\\', __DIR__ . '/Mock');
 error_reporting(E_ALL);
+
+// load for p($var); e();
+(new \BEAR\Package\Dev\Dev)->loadDevFunctions();
 
 $_ENV['TEST_DIR'] = __DIR__;
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
