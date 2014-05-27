316c60b8a0eb1cc7eb94de57343ceac3f6739a52
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-28 06:02:14 +0900

    error name to error message

--- src-dev/Dev/Dev.php
+++ src-dev/Dev/Dev.php
@@ -52,6 +52,27 @@ class Dev
     private $sapiName;
 
     /**
+     * @var array
+     */
+    private $errorName = [
+        E_ERROR => 'E_ERROR',
+        E_WARNING => 'E_WARNING',
+        E_PARSE => 'E_PARSE',
+        E_NOTICE => 'E_NOTICE',
+        E_CORE_ERROR => 'E_CORE_ERROR',
+        E_CORE_WARNING => 'E_CORE_WARNING',
+        E_COMPILE_ERROR => 'E_COMPILE_ERROR',
+        E_COMPILE_WARNING => 'E_COMPILE_WARNING',
+        E_USER_ERROR => 'E_USER_ERROR',
+        E_USER_WARNING => 'E_USER_WARNING',
+        E_USER_NOTICE => 'E_USER_NOTICE',
+        E_STRICT => 'E_STRICT',
+        E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+        E_DEPRECATED => 'E_DEPRECATED',
+        E_USER_DEPRECATED => 'E_USER_DEPRECATED'
+    ];
+
+    /**
      * @param AppInterface $app
      * @param string       $appDir
      *
@@ -167,7 +188,11 @@ class Dev
 
                 return;
             }
-            throw new \ErrorException($errStr, 0, $errNo, $errFile, $errLine);
+            $msg = sprintf('%s: %s',
+                $this->errorName[$errNo],
+                $errStr
+            );
+            throw new \ErrorException($msg, 0, $errNo, $errFile, $errLine);
         });
 
         return $this;
