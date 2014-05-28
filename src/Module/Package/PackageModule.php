ab3d46c33346755b608ec26c867f24fc301f2601
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-28 12:45:07 +0900

    move ExceptionHandle to src-dev

--- src/Module/Package/PackageModule.php
+++ src/Module/Package/PackageModule.php
@@ -5,6 +5,7 @@ use BEAR\Package\Module as Package;
 use BEAR\Package\Provide as Provide;
 use BEAR\Sunday\Module as Sunday;
 use BEAR\Resource\Module as Resource;
+use BEAR\Package\Dev\Module as DevPackage;
 
 use Ray\Di\AbstractModule;
 
@@ -57,7 +58,7 @@ class PackageModule extends AbstractModule
         $this->install(new Package\Cache\CacheAspectModule($this));
         $this->install(new Package\Di\DiCompilerModule($this));
         $this->install(new Package\Di\DiModule($this));
-        $this->install(new Package\ExceptionHandle\HandleModule);
+        $this->install(new DevPackage\ExceptionHandle\ExceptionHandleModule);
         $this->install(new Package\Resource\ResourceModule($this->config['app_name'], $this->config['resource_dir']));
 
         // Resource Module
