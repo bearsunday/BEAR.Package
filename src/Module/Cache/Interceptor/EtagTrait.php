d5152a34b924e56d44d7b681350fb43e5c1bc948
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-05-30 20:11:11 +0900

    fix cache id
    extract ArrayObject to array()
    related:  Allow change arguments by interceptor. #14
    https://github.com/koriym/Ray.Aop/pull/14

--- src/Module/Cache/Interceptor/EtagTrait.php
+++ src/Module/Cache/Interceptor/EtagTrait.php
@@ -18,7 +18,14 @@ trait EtagTrait
      */
     public function getEtag($object, $args)
     {
+        $args = ($args instanceof \ArrayObject) ? $args->getArrayCopy() : $args;
         $etag = crc32(get_class($object) . serialize($args));
+        error_log(sprintf(
+            'etag:%s class:%s args:$s tag:%s',
+            $etag,
+            get_class($object),
+            serialize($args)
+        ));
 
         return $etag;
     }
