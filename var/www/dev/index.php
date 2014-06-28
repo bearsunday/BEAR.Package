902c66e35d71aeebc26cce25c25241f3e72fe202
kenjis <kenji.uui@gmail.com>
2014-06-29 08:31:53 +0900

    Fix typo

--- var/www/dev/index.php
+++ var/www/dev/index.php
@@ -23,14 +23,14 @@ $contentsForLayout =<<<EOT
         <span class="label label-success">GET</span>
         <ul class="">
             <li><a href="/dev/log/"><span class="glyphicon glyphicon-list"></span> Resource Log</a></li>
-            <li><a href="/dev/resource/"><span class="glyphicon glyphicon-list"></span> </i> Resource List</a></li>
+            <li><a href="/dev/resource/"><span class="glyphicon glyphicon-list"></span> Resource List</a></li>
             <li><a href="/dev/di/"><span class="glyphicon glyphicon-list"></span> Di Log</a></li>
             <li><a href="/dev/app/"><span class="glyphicon glyphicon-list"></span> Application Object Graph</a></li>
         </ul>
         <hr>
         <span class="label label-danger">POST</span>
         <ul>
-            <li><a href="/dev/resource/new""><span class="glyphicon glyphicon-plus"></span> Add Resource</a></li>
+            <li><a href="/dev/resource/new"><span class="glyphicon glyphicon-plus"></span> Add Resource</a></li>
         </ul>
         <hr>
         <span class="label label-warning">PUT</span>
