d1e85b678ad7198c8f8dd3eae7c3dd2aa55591a3
kenjis <kenji.uui@gmail.com>
2014-07-15 06:09:23 +0900

    Fix coding style
    using use statements

--- src/Module/Session/AuraSession/SessionProvider.php
+++ src/Module/Session/AuraSession/SessionProvider.php
@@ -3,16 +3,21 @@
 namespace BEAR\Package\Module\Session\AuraSession;
 
 use Ray\Di\ProviderInterface;
+use Aura\Session\Manager;
+use Aura\Session\SegmentFactory;
+use Aura\Session\CsrfTokenFactory;
+use Aura\Session\Randval;
+use Aura\Session\Phpfunc;
 
 class SessionProvider implements ProviderInterface
 {
     public function get()
     {
-        return new \Aura\Session\Manager(
-            new \Aura\Session\SegmentFactory,
-            new \Aura\Session\CsrfTokenFactory(
-                new \Aura\Session\Randval(
-                    new \Aura\Session\Phpfunc
+        return new Manager(
+            new SegmentFactory,
+            new CsrfTokenFactory(
+                new Randval(
+                    new Phpfunc
                 )
             ),
             $_COOKIE
