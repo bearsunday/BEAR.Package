31009a3d2c1209d0200fa9032a80f11dc0061861
kenjis <kenji.uui@gmail.com>
2014-07-15 06:11:56 +0900

    Add license doc comment

--- src/Module/Session/AuraSession/SessionProvider.php
+++ src/Module/Session/AuraSession/SessionProvider.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
 namespace BEAR\Package\Module\Session\AuraSession;
 
 use Ray\Di\ProviderInterface;
