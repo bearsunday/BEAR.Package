87fb0e05cf128d9834e8ca84a2a84bc474a0e9f6
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-08 03:05:46 +0900

    add BootstrapTest

--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BEAR\Package\Bootstrap;
+namespace BEAR\Package;
 
 use Doctrine\Common\Cache\ArrayCache;
 use BEAR\Sunday\Extension\Application\AppInterface;
@@ -10,7 +10,7 @@ class BootstrapTest extends \PHPUnit_Framework_TestCase
 {
     public function testNewApp()
     {
-        $app = (new Bootstrap)->newApp('FakeVendor\HelloWorld', 'prod-app', new ArrayCache);
+        $app = (new Bootstrap)->newApp(new AppMeta('FakeVendor\HelloWorld'), 'prod-app', new ArrayCache);
         $this->assertInstanceOf(AppInterface::class, $app);
         $expect = ['FakeVendor\HelloWorld\Module\AppModule', 'FakeVendor\HelloWorld\Module\ProdModule'];
         $this->assertSame($expect, AppModule::$modules);
