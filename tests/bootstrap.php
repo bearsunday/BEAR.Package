c66ab773e116d1eec3ac409a423bffeaae74dddf
koriym <akihito.koriyama@gmail.com>
2014-12-10 11:20:02 +0900

    add annotation loader at tests/bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,7 @@ $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /** @var $loader \Composer\Autoload\ClassLoader */
 $loader->addPsr4('BEAR\Package\\', __DIR__);
 $loader->addPsr4('FakeVendor\HelloWorld\\', __DIR__ . '/Fake/FakeVendor/HelloWorld/src');
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 $_ENV['TEST_DIR'] = __DIR__;
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
