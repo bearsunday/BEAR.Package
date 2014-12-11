edd4c33322cada20c53eda2474e996e02c3bfd7d
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-11 23:51:49 +0900

    add ApiModule

---
+++ src/Context/ApiModule.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Context;
+
+use BEAR\Package\Provide\Router\ApiRouter;
+use BEAR\Sunday\Extension\Router\RouterInterface;
+use BEAR\Sunday\Extension\Transfer\TransferInterface;
+use Ray\Di\AbstractModule;
+
+class ApiModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->bind(RouterInterface::class)->to(ApiRouter::class);
+        $this->bind(RouterInterface::class)->annotatedWith('original')->to(ApiRouter::class);
+    }
+}
