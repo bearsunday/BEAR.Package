8153befdd34dc8995a9d14ab45da792caed6245e
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-15 01:24:03 +0900

    move fake-app

--- tests/AppMetaTest.php
+++ tests/AppMetaTest.php
@@ -9,7 +9,7 @@ class AppMetaTest extends \PHPUnit_Framework_TestCase
         $appName = 'FakeVendor\HelloWorld';
         $appMeta = new AppMeta($appName);
         $this->assertSame($appName, $appMeta->name);
-        $expectAppDir = __DIR__ . '/Fake/FakeVendor/HelloWorld';
+        $expectAppDir = __DIR__ . '/Fake/fake-app';
         $this->assertSame($expectAppDir, $appMeta->appDir);
         $this->assertSame($expectAppDir . '/var/tmp', $appMeta->tmpDir);
         $this->assertSame($expectAppDir . '/var/log', $appMeta->logDir);
