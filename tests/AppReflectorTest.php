abdecc8cd5559a602c2db099b64d3fec687211ce
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-20 01:42:01 +0900

    fix tests

--- tests/AppReflectorTest.php
+++ tests/AppReflectorTest.php
@@ -13,17 +13,17 @@ class AppReflectorTest extends \PHPUnit_Framework_TestCase
             $files[] = $file;
         }
         $expect = [
-            'FakeVendor\HelloWorld\Resource\App\FakeUser',
             'FakeVendor\HelloWorld\Resource\App\One',
             'FakeVendor\HelloWorld\Resource\App\Two',
+            'FakeVendor\HelloWorld\Resource\App\User',
             'FakeVendor\HelloWorld\Resource\Page\Index',
             'FakeVendor\HelloWorld\Resource\App\Sub\Three',
             'FakeVendor\HelloWorld\Resource\App\Sub\Sub\Four'        ];
         $this->assertSame($expect, $classes);
         $expect = [
-            $appMeta->appDir . '/src/Resource/App/FakeUser.php',
             $appMeta->appDir . '/src/Resource/App/One.php',
             $appMeta->appDir . '/src/Resource/App/Two.php',
+            $appMeta->appDir . '/src/Resource/App/User.php',
             $appMeta->appDir . '/src/Resource/Page/Index.php',
             $appMeta->appDir . '/src/Resource/App/Sub/Three.php',
             $appMeta->appDir . '/src/Resource/App/Sub/Sub/Four.php'
