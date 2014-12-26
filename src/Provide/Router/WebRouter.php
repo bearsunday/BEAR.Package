4a3acb3fd385cff1f54aa593cc2b7b0b4823d1d2
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-12-26 20:03:24 +0900

    [#161] package web router for api

---
+++ src/Provide/Router/WebRouter.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Router;
+
+use BEAR\Sunday\Extension\Router\RouterInterface;
+use BEAR\Sunday\Extension\Router\RouterMatch;
+use Ray\Di\Di\Inject;
+use Ray\Di\Di\Named;
+
+class WebRouter implements RouterInterface
+{
+    /**
+     * @var string
+     */
+    private $defaultRouteHost  = 'page://self';
+
+    /**
+     * @Named("default_route_host")
+     * @Inject(optional=true)
+     */
+    public function setRouteHost($default_route_host)
+    {
+        $this->defaultRouteHost = $default_route_host;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function match(array $globals = [])
+    {
+        $request = new RouterMatch;
+        $method = strtolower($globals['_SERVER']['REQUEST_METHOD']);
+        list($request->method, $request->path, $request->query) = [
+            $method,
+            $this->defaultRouteHost . parse_url($globals['_SERVER']['REQUEST_URI'], PHP_URL_PATH),
+            ($method === 'get') ? $globals['_GET'] : $globals['_POST']
+        ];
+
+        return $request;
+    }
+}
