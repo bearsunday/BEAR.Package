60dc00cc45ab8c4ce8dd7a0b3086fffe5e6cb533
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-01-14 20:44:27 +0900

    use profile link for demo page

--- docs/demo-app/src/Resource/App/User.php
+++ docs/demo-app/src/Resource/App/User.php
@@ -5,14 +5,11 @@ namespace MyVendor\MyApp\Resource\App;
 use BEAR\Resource\Annotation\Embed;
 use BEAR\Resource\Annotation\Link;
 use BEAR\Resource\ResourceObject;
-use BEAR\Sunday\Inject\ResourceInject;
 
 class User extends ResourceObject
 {
-    use ResourceInject;
-
     /**
-     * @Link(rel="contact", href="/contact{?id}")
+     * @Link(rel="profile", href="/profile{?id}")
      * @Embed(rel="website", src="app://self/website{?id}")
      * @Embed(rel="contact", src="app://self/contact{?id}")
      */
