0e0d7cdd491528b56a9b87a92d99c4501358aae0
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-01-22 17:11:28 +0900

    add EtagInterceptor module

---
+++ src/Provide/Transfer/EtagResponseInterceptor.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the *** package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Transfer;
+
+use Doctrine\Common\Annotations\Reader;
+use Ray\Aop\MethodInterceptor;
+use Ray\Aop\MethodInvocation;
+
+class EtagResponseInterceptor implements MethodInterceptor
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function invoke(MethodInvocation $invocation)
+    {
+        $server = $invocation->getArguments()[1];
+        $headers = $invocation->getThis()->headers;
+        if (isset($headers['Etag']) && isset($server['HTTP_IF_NONE_MATCH']) && stripslashes($server['HTTP_IF_NONE_MATCH']) === $headers['Etag']) {
+            goto NOT_MODIFIED_304;
+        }
+        if (isset($headers['Last-Modified']) && isset($server['HTTP_IF_MODIFIED_SINCE']) && $server['HTTP_IF_MODIFIED_SINCE'] === $headers['Last-Modified']) {
+            goto NOT_MODIFIED_304;
+        }
+
+        return $invocation->proceed();
+
+        NOT_MODIFIED_304: {
+            http_response_code(304);
+            header('Cache-Control: public');
+        }
+    }
+}
---
+++ src/Provide/Transfer/EtagResponseModule.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Transfer;
+
+use BEAR\Package\Annotation\Etag;
+use Ray\Di\AbstractModule;
+
+class EtagResponseModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->bindInterceptor(
+            $this->matcher->annotatedWith(Etag::class),
+            $this->matcher->startsWith('transfer'),
+            [EtagResponseInterceptor::class]
+        );
+    }
+}
