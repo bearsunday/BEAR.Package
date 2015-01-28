e322f02c83cb12a6e2dcb198c9d7fe21cccd77f2
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-01-28 11:29:35 +0900

    add OverrideMethod

---
+++ tests/Provide/Router/file_get_contents.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BEAR\Package\Provide\Router;
+
+function file_get_contents($filename, $flags = null, $context = null, $offset = null, $maxlen = null)
+{
+    return "name=kuma";
+}
