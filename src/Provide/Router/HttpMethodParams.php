997aeec2dc0023ab0cd45eb191c12a7297dd0325
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-02-03 01:10:28 +0900

    change to HttpMethodParams

---
+++ src/Provide/Router/HttpMethodParams.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * This file is part of the BEAR.Sunday package
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace BEAR\Package\Provide\Router;
+
+final class HttpMethodParams
+{
+    const CONTENT_TYPE = 'Content-Type';
+
+    const FORM_URL_ENCODE = 'application/x-www-form-urlencoded';
+
+    const APPLICATION_JSON = 'application/json';
+
+    /**
+     * Return http method and parameters
+     *
+     * 'parameters' change by method.
+     * get method return $_GET, post method return $_POST
+     * patch | put | delete  return parsed 'php://input' value if form-urlencoded or json content
+     *
+     * @param array $server $_SERVER
+     * @param array $get    $_GET
+     * @param array $post   $_POST
+     *
+     * @return array [$method, $params]
+     */
+    public function get(array $server, array $get, array $post) {
+
+        // set the original value
+        $method = strtolower($server['REQUEST_METHOD']);
+
+        // early return on GET
+        if ($method === 'get') {
+            return ['get', $get];
+        }
+        // must be a POST to do an override
+        $override = $this->getOverRideMethod($server, $post);
+        if ($override) {
+            return [$override, $this->getParams($method, $get, $post, $server)];
+        }
+        if ($method === 'post') {
+            return ['post', $post];
+        }
+        // put / patch /delete
+        return [$method, $this->getParams($method, $post, $server)];
+    }
+
+    /**
+     * HTTP Method override
+     *
+     * @param array $server
+     * @param array &$post
+     *
+     * @return bool is override ?
+     */
+    private function getOverRideMethod(array $server, array &$post)
+    {
+        // look for override in post data
+        if (isset($post['_method'])) {
+            $method =  strtolower($post['_method']);
+            unset($post['_method']);
+
+            return $method;
+        }
+
+        // look for override in headers
+        if (isset($server['HTTP_X_HTTP_METHOD_OVERRIDE'])) {
+            return strtolower($server['HTTP_X_HTTP_METHOD_OVERRIDE']);
+        }
+
+        return false;
+    }
+
+    /**
+     * Return request parameters
+     *
+     * @param string $method
+     * @param array  $post
+     * @param array  $server
+     *
+     * @return array
+     */
+    private function getParams($method, array $post, array $server)
+    {
+        if ($method === 'put' || $method === 'patch'  || $method === 'delete') {
+
+            return $this->phpInput($server);
+        }
+
+        return $post;
+    }
+
+    /**
+     * Take 'php://input' as input in form-urlencoded or json
+     *
+     * @param array $server
+     *
+     * @return array
+     */
+    private function phpInput(array $server)
+    {
+        if (! isset($server['Content-Type'])) {
+            return [];
+        }
+        $isFormUrlEncoded = strpos($server[self::CONTENT_TYPE], self::FORM_URL_ENCODE) !== false;
+        if($isFormUrlEncoded) {
+            parse_str(file_get_contents('php://input'), $put);
+
+            return $put;
+        }
+        $isApplicationJson = strpos($server[self::CONTENT_TYPE], self::APPLICATION_JSON) !== false;
+        if ($isApplicationJson) {
+            return json_decode(file_get_contents('php://input'));
+        }
+
+        return [];
+    }
+}
--- tests/Provide/Router/HttpMethodParamsTest.php
+++ tests/Provide/Router/HttpMethodParamsTest.php
@@ -4,14 +4,14 @@ namespace BEAR\Package\Provide\Router;
 
 require __DIR__ . '/file_get_contents.php';
 
-class OverrideMethodTest extends \PHPUnit_Framework_TestCase
+class HttpMethodParamsTest extends \PHPUnit_Framework_TestCase
 {
     public function testGet()
     {
         $server = ['REQUEST_METHOD' => 'GET'];
         $get = ['id' => '1'];
         $post = [];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method, $params) = (new HttpMethodParams)->get($server, $get, $post);
         $this->assertSame('get', $method);
         $this->assertSame(['id' => '1'], $params);
     }
@@ -21,98 +21,86 @@ class OverrideMethodTest extends \PHPUnit_Framework_TestCase
         $server = ['REQUEST_METHOD' => 'POST'];
         $get = [];
         $post = ['id' => '1'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method, $params) = (new HttpMethodParams)->get($server, $get, $post);
         $this->assertSame('post', $method);
         $this->assertSame(['id' => '1'], $params);
     }
 
     public function testPut()
     {
-        $server = ['REQUEST_METHOD' => 'PUT'];
+        $server = ['REQUEST_METHOD' => 'PUT', HttpMethodParams::CONTENT_TYPE => HttpMethodParams::FORM_URL_ENCODE];
         $get = ['name' => 'bear'];
         $post = ['name' => 'sunday'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method, $params) = (new HttpMethodParams)->get($server, $get, $post);
         $this->assertSame('put', $method);
         $this->assertSame(['name' => 'kuma'], $params);
     }
 
     public function testPatch()
     {
-        $server = ['REQUEST_METHOD' => 'PATCH'];
+        $server = ['REQUEST_METHOD' => 'PATCH', HttpMethodParams::CONTENT_TYPE => HttpMethodParams::FORM_URL_ENCODE];
         $get = ['name' => 'bear'];
         $post = ['name' => 'sunday'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method, $params) = (new HttpMethodParams)->get($server, $get, $post);
         $this->assertSame('patch', $method);
         $this->assertSame(['name' => 'kuma'], $params);
     }
 
     public function testDelete()
     {
-        $server = ['REQUEST_METHOD' => 'DELETE'];
+        $server = ['REQUEST_METHOD' => 'DELETE', HttpMethodParams::CONTENT_TYPE => HttpMethodParams::FORM_URL_ENCODE];
         $get = ['name' => 'bear'];
         $post = ['name' => 'sunday'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method, $params) = (new HttpMethodParams)->get($server, $get, $post);
         $this->assertSame('delete', $method);
         $this->assertSame(['name' => 'kuma'], $params);
     }
 
     public function testOverridePut()
     {
-        $server = ['REQUEST_METHOD' => 'POST'];
-        $get = ['name' => 'bear'];
-        $post = ['name' => 'sunday', '_method' => 'PUT'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        $server = ['REQUEST_METHOD' => 'POST', HttpMethodParams::CONTENT_TYPE => HttpMethodParams::FORM_URL_ENCODE];
+        $post = ['_method' => 'PUT'];
+        list($method, ) = (new HttpMethodParams)->get($server, [], $post);
         $this->assertSame('put', $method);
-        $this->assertSame(['name' => 'sunday'], $params);
     }
 
     public function testOverridePatch()
     {
-        $server = ['REQUEST_METHOD' => 'POST'];
-        $get = ['name' => 'bear'];
-        $post = ['name' => 'sunday', '_method' => 'PATCH'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        $server = ['REQUEST_METHOD' => 'POST', HttpMethodParams::CONTENT_TYPE => HttpMethodParams::FORM_URL_ENCODE];
+        $post = ['_method' => 'PATCH'];
+        list($method, ) = (new HttpMethodParams)->get($server, [], $post);
         $this->assertSame('patch', $method);
-        $this->assertSame(['name' => 'sunday'], $params);
     }
 
     public function testOverrideDelete()
     {
-        $server = ['REQUEST_METHOD' => 'POST'];
-        $get = ['name' => 'bear'];
-        $post = ['name' => 'sunday', '_method' => 'DELETE'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        $server = ['REQUEST_METHOD' => 'POST', HttpMethodParams::CONTENT_TYPE => HttpMethodParams::FORM_URL_ENCODE];
+        $post = ['_method' => 'DELETE'];
+        list($method, ) = (new HttpMethodParams)->get($server, [], $post);
         $this->assertSame('delete', $method);
-        $this->assertSame(['name' => 'sunday'], $params);
     }
 
     public function testOverrideHeaderPut()
     {
         $server = ['REQUEST_METHOD' => 'POST', 'HTTP_X_HTTP_METHOD_OVERRIDE' => 'PUT'];
-        $get = ['name' => 'bear'];
         $post = ['name' => 'sunday'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method) = (new HttpMethodParams)->get($server, [], $post);
         $this->assertSame('put', $method);
-        $this->assertSame(['name' => 'sunday'], $params);
     }
 
     public function testOverrideHeaderPatch()
     {
         $server = ['REQUEST_METHOD' => 'POST', 'HTTP_X_HTTP_METHOD_OVERRIDE' => 'PATCH'];
-        $get = ['name' => 'bear'];
         $post = ['name' => 'sunday'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method) = (new HttpMethodParams)->get($server, [], $post);
         $this->assertSame('patch', $method);
-        $this->assertSame(['name' => 'sunday'], $params);
     }
 
     public function testOverrideHeaderDelete()
     {
         $server = ['REQUEST_METHOD' => 'POST', 'HTTP_X_HTTP_METHOD_OVERRIDE' => 'DELETE'];
-        $get = ['name' => 'bear'];
         $post = ['name' => 'sunday'];
-        list($method, $params) = (new OverrideMethod)->get($server, $get, $post);
+        list($method) = (new HttpMethodParams)->get($server, [], $post);
         $this->assertSame('delete', $method);
-        $this->assertSame(['name' => 'sunday'], $params);
     }
 }
