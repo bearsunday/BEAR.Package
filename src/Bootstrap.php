676aa683a82bc10cc8d34522d6c3f65b74227320
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-02-27 05:45:12 +0900

    refactor Bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -11,12 +11,11 @@ use BEAR\Sunday\Extension\Application\AppInterface;
 use Doctrine\Common\Cache\ApcCache;
 use Doctrine\Common\Cache\Cache;
 use Doctrine\Common\Cache\FilesystemCache;
+use Ray\Di\AbstractModule;
 use Ray\Di\Injector;
 
 final class Bootstrap
 {
-    const PACKAGE_MODULE_PATH = 'BEAR\Package\Context\\';
-
     /**
      * @param AbstractAppMeta $appMeta
      * @param string          $contexts
@@ -27,32 +26,39 @@ final class Bootstrap
     public function newApp(AbstractAppMeta $appMeta, $contexts, Cache $cache = null)
     {
         if (is_null($cache)) {
-            $cache = $this->contextCache($appMeta);
+            $cache = function_exists('apc_fetch') ? new ApcCache : new FilesystemCache($appMeta->tmpDir);
+            $cache->setNamespace(filemtime($appMeta->appDir . '/src/.'));
         }
         $app = $cache->fetch($contexts);
         if ($app) {
             return $app;
         }
-        $contextsArray = array_reverse(explode('-', $contexts));
-        $module = null;
-        foreach ($contextsArray as $context) {
-            $class = $appMeta->name . '\Module\\' . ucwords($context) . 'Module';
-            if (! class_exists($class)) {
-                $class = self::PACKAGE_MODULE_PATH . ucwords($context) . 'Module';
-            }
-            $module =  new $class($module);
-        }
-        $app = (new Injector($module, $appMeta->tmpDir))->getInstance(AppInterface::class);
+        $app = $this->createAppInstance($appMeta ,$contexts);
         $cache->save($contexts, $app);
 
         return $app;
     }
 
-    public function contextCache(AbstractAppMeta $appMeta)
+    /**
+     * @param AbstractAppMeta $appMeta
+     * @param string          $contexts
+     *
+     * @return AppInterface
+     */
+    private function createAppInstance(AbstractAppMeta $appMeta ,$contexts)
     {
-        $cache = function_exists('apc_fetch') ? new ApcCache : new FilesystemCache($appMeta->tmpDir);
-        $cache->setNamespace(filemtime($appMeta->appDir . '/src/.'));
+        $contextsArray = array_reverse(explode('-' ,$contexts));
+        $module = null;
+        foreach ($contextsArray as $context) {
+            $class = $appMeta->name . '\Module\\' . ucwords($context) . 'Module';
+            if (! class_exists($class)) {
+                $class = 'BEAR\Package\Context\\' . ucwords($context) . 'Module';
+            }
+            /** @var $module AbstractModule */
+            $module = new $class($module);
+        }
+        $app = (new Injector($module ,$appMeta->tmpDir))->getInstance(AppInterface::class);
 
-        return $cache;
+        return $app;
     }
 }
