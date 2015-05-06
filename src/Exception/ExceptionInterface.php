d73fc761ec096ee7ae537267fd6eac110492faef
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-07 00:09:54 +0900

    add package basic exceptions

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Package\Exception;
+
+interface ExceptionInterface
+{
+}
