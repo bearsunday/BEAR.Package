b4e13f82f19841e2abf0e1d878ab0d789463e155
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-07 03:21:02 +0900

    replace std-in in cli input

---
+++ src/Annotation/StdIn.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Package\Annotation;
+use Ray\Di\Di\Qualifier;
+
+/**
+ * @Annotation
+ * @Target("METHOD")
+ * @Qualifier
+ */
+final class StdIn
+{
+}
