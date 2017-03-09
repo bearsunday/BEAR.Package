e3c4309b20ab7eed6a2f4c391b1d3f3923d6c65e
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-10 06:46:45 +0900

    use autoload-dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
+// loader
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /* @var $loader \Composer\Autoload\ClassLoader */
+$loader->addPsr4('BEAR\Package\\', __DIR__);
+$loader->addPsr4('BEAR\Package\\', __DIR__ . '/Fake');
+$loader->addPsr4('FakeVendor\HelloWorld\\', __DIR__ . '/Fake/fake-app/src');
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 $_ENV['TEST_DIR'] = __DIR__;
