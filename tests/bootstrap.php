b647bf9d06bb37fe5c400d5ca0611fa902020ffd
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-10 06:42:02 +0900

    diet composer.json

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,7 @@
 <?php
 
-// loader
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 /* @var $loader \Composer\Autoload\ClassLoader */
-$loader->addPsr4('BEAR\Package\\', __DIR__);
-$loader->addPsr4('BEAR\Package\\', __DIR__ . '/Fake');
-$loader->addPsr4('FakeVendor\HelloWorld\\', __DIR__ . '/Fake/fake-app/src');
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 $_ENV['TEST_DIR'] = __DIR__;
