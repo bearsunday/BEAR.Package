2b46822c2d2e9e75160e3f4c21421a0c4c01badf
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-10 13:13:26 +0900

    remove unused Fake/

--- tests/Fake/fake-app/var/conf/aura.route.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\BEAR\Package\Provide\Router\AuraRouterModuleTest::$routerClass = $router;
-
-/* @var $router \Aura\Router\RouteCollection */
-$router->add('/user', '/user/{id}')->addValues(['path' => '/user']);
-$router->add('/friend', '/friend/{id}')->addValues(['path' => '/friend']);
