206caf5b95d05c4fe21d27dfef575b7d61a9ae56
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-13 21:25:45 +0900

    refactor bootstrap

--- docs/demo-app/bootstrap/bootstrap.php
+++ docs/demo-app/bootstrap/bootstrap.php
@@ -1,37 +1,31 @@
 <?php
-
-/**
- * @global string $context
- */
 namespace MyVendor\MyApp;
 
 use BEAR\Package\Bootstrap;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 load: {
+    // require dirname(__DIR__) . '/var/lib/xhprof.php';
     $loader = require dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php';
     /* @var $loader \Composer\Autoload\ClassLoader */
     $loader->addPsr4(__NAMESPACE__ . '\\', dirname(__DIR__) . '/src');
     AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 }
-
-route: {
+boot: {
+    /* @global $context */
     $app = (new Bootstrap)->getApp(__NAMESPACE__, $context);
-    /* @var $app AbstractApp \BEAR\Sunday\Extension\Application\AbstractApp */
     $request = $app->router->match($GLOBALS, $_SERVER);
 }
-
 try {
     // resource request
     $page = $app->resource
         ->{$request->method}
         ->uri($request->path)
         ->withQuery($request->query)
+        ->eager
         ->request();
-    /* @var $page \BEAR\Resource\Request */
-
-    // representation transfer
-    $page()->transfer($app->responder, $_SERVER);
+    /* @var $page \BEAR\Resource\ResourceObject */
+    $page->transfer($app->responder, $_SERVER);
     exit(0);
 } catch (\Exception $e) {
     $app->error->handle($e, $request)->transfer();
