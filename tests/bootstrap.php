2e326aef4040228947076dd0ba3d6ce81ab0f491
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-03-15 20:37:58 +0900

    remove unlink script in test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,19 +11,3 @@ $loader->addPsr4('FakeVendor\HelloWorld\\', __DIR__ . '/Fake/fake-app/src');
 $_ENV['TEST_DIR'] = __DIR__;
 $_ENV['TMP_DIR'] = __DIR__ . '/tmp';
 $_ENV['PACKAGE_DIR'] = dirname(__DIR__);
-
-$unlink = function ($path) use (&$unlink) {
-    foreach (glob(rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . '*') as $file) {
-        is_dir($file) ? $unlink($file) : unlink($file);
-        @rmdir($file);
-    }
-};
-
-$unlink($_ENV['TMP_DIR']);
-$unlink(__DIR__ . '/Fake/fake-app/var/tmp');
-
-register_shutdown_function(function () use ($unlink) {
-    $unlink($_ENV['TMP_DIR']);
-    $unlink(__DIR__ . '/Fake/fake-app/var/tmp');
-    $unlink(__DIR__ . '/Fake/fake-app/var/log');
-});
