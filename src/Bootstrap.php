478b35e75f3daf7b3388841b42fad4388a161c45
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-04-29 15:24:05 +0900

    remove unused imports

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,7 +8,6 @@ namespace BEAR\Package;
 
 use BEAR\AppMeta\AbstractAppMeta;
 use BEAR\AppMeta\AppMeta;
-use BEAR\Resource\Resource;
 use BEAR\Resource\ResourceInterface;
 use BEAR\Sunday\Extension\Application\AbstractApp;
 use BEAR\Sunday\Extension\Application\AppInterface;
