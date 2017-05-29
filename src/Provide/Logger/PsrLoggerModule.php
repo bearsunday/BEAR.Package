ce34cb79d42791bec7d6905f3313e7e5e829c55a
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-05-29 17:47:54 +0900

    Add PsrLoggerModule (#240)
    * add PsrLoggerModule
    
    * remove unused import
    
    * use StreamHandler as default logger

---
+++ src/Provide/Logger/PsrLoggerModule.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the BEAR.Package package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BEAR\Package\Provide\Logger;
+
+use Psr\Log\LoggerInterface;
+use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
+
+class PsrLoggerModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->bind(LoggerInterface::class)->toProvider(MonologProviver::class)->in(Scope::SINGLETON);
+    }
+}
