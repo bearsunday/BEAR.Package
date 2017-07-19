acfc8f34cbd6a83c1e19c521ce76ef211fac004e
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-07-19 20:24:11 +0900

    use ChainCache(apc + file) for prod

--- tests/Fake/fake-app/src/Module/ProdModule.php
+++ tests/Fake/fake-app/src/Module/ProdModule.php
@@ -1,6 +1,12 @@
 <?php
+/**
+ * This file is part of the BEAR.Package package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 namespace FakeVendor\HelloWorld\Module;
 
+use BEAR\Package\Context\ProdModule as PackageProdModule;
 use Ray\Di\AbstractModule;
 
 class ProdModule extends AbstractModule
@@ -11,5 +17,6 @@ class ProdModule extends AbstractModule
     protected function configure()
     {
         AppModule::$modules[] = get_class($this);
+        $this->install(new PackageProdModule());
     }
 }
