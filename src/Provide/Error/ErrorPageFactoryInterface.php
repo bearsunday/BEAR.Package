25b535a6555479d02ae296687bb7234b6fe77ada
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-14 16:10:20 +0900

    remove phpdoc

--- src/Provide/Error/ErrorPageFactoryInterface.php
+++ src/Provide/Error/ErrorPageFactoryInterface.php
@@ -12,9 +12,6 @@ use BEAR\Sunday\Extension\Router\RouterMatch;
 interface ErrorPageFactoryInterface
 {
     /**
-     * @param \Exception  $e
-     * @param RouterMatch $request
-     *
      * @return ResourceObject
      */
     public function newInstance(\Exception $e, RouterMatch $request);
