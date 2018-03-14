95458848df70dc99500451962aaa6b7f2fab4319
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-14 11:24:25 +0900

    update CompilerTest for Ray.Compiler v1.1.7

--- tests/CompilerTest.php
+++ tests/CompilerTest.php
@@ -14,9 +14,11 @@ class CompilerTest extends TestCase
     {
         $compiledFile1 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/__FakeVendor_HelloWorld_Resource_Page_Index-.php';
         $compiledFile2 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/FakeVendor_HelloWorld_Resource_Page_Index-';
+        $compiledFile3 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/FakeVendor_HelloWorld_Resource_Page_Index-.php'; // php7.1 Ray.Compiler v1.1.7
         @unlink($compiledFile1);
         @unlink($compiledFile2);
+        @unlink($compiledFile3);
         (new Compiler)->__invoke('FakeVendor\HelloWorld', 'prod-cli-app', __DIR__ . '/Fake/fake-app');
-        $this->assertTrue(file_exists($compiledFile1) || file_exists($compiledFile2));
+        $this->assertTrue(file_exists($compiledFile1) || file_exists($compiledFile2) || $compiledFile3);
     }
 }
