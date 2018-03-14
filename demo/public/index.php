9cbbee786ddc6fddf8f065b3cbfe393b17be2459
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-03-14 14:10:11 +0900

    refactor demo code

--- demo/public/index.php
+++ demo/public/index.php
@@ -1,13 +1,11 @@
 <?php
 use BEAR\Package\Bootstrap;
-use BEAR\Sunday\Extension\Application\AbstractApp;
 
-require dirname(__DIR__) . '/load.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 $context = PHP_SAPI === 'cli' ? 'cli-hal-app' : 'hal-app';
 
 $app = (new Bootstrap)->getApp('MyVendor\MyApp', $context);
-/* @var $app AbstractApp */
 $request = $app->router->match($GLOBALS, $_SERVER);
 try {
     $page = $app
