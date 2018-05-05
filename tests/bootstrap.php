93b85dcb3ad6a817f0008383bc96aa088b8f52ce
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-05-05 16:57:56 +0900

    function delete_dir to soothe phpstan

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,14 +9,6 @@ $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 require __DIR__ . '/hash.php';
 
-function delete_dir($dir)
-{
-    foreach (glob($dir . '/*') as $file) {
-        is_dir($file) ? delete_dir($file) : unlink($file);
-        @rmdir($file);
-    }
-}
-
 delete_dir(__DIR__ . '/tmp');
 delete_dir(__DIR__ . '/Fake/fake-app/var/tmp');
 delete_dir(__DIR__ . '/Fake/fake-app/var/log');
