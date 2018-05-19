0ad8305243dad35f0714e8745f4649ecc9a9129e
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-05-19 20:38:06 +0900

    force delete

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,7 @@ use BEAR\Package\Unlink;
 require dirname(__DIR__) . '/vendor/autoload.php';
 require __DIR__ . '/hash.php';
 
-(new Unlink)(__DIR__ . '/tmp');
-(new Unlink)(__DIR__ . '/Fake/fake-app/var/tmp');
-(new Unlink)(__DIR__ . '/Fake/fake-app/var/log');
-(new Unlink)(dirname(__DIR__) . '/var/tmp');
+(new Unlink)->force(__DIR__ . '/tmp');
+(new Unlink)->force(__DIR__ . '/Fake/fake-app/var/tmp');
+(new Unlink)->force(__DIR__ . '/Fake/fake-app/var/log');
+(new Unlink)->force(dirname(__DIR__) . '/var/tmp');
