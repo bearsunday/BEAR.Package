2b16e24f27291d5c946a31f58e483f831ccc5af1
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-13 14:54:12 +0900

    update testInvoke

--- tests/CompilerTest.php
+++ tests/CompilerTest.php
@@ -12,13 +12,9 @@ class CompilerTest extends TestCase
 {
     public function testInvoke()
     {
-        $compiledFile1 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/__FakeVendor_HelloWorld_Resource_Page_Index-.php';
-        $compiledFile2 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/FakeVendor_HelloWorld_Resource_Page_Index-';
-        $compiledFile3 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/FakeVendor_HelloWorld_Resource_Page_Index-.php'; // php7.1 Ray.Compiler v1.1.7
+        $compiledFile1 = __DIR__ . '/Fake/fake-app/var/tmp/prod-cli-app/di/FakeVendor_HelloWorld_Resource_Page_Index-.php';
         @unlink($compiledFile1);
-        @unlink($compiledFile2);
-        @unlink($compiledFile3);
-        (new Compiler)->__invoke('FakeVendor\HelloWorld', 'prod-cli-app', __DIR__ . '/Fake/fake-app');
-        $this->assertTrue(file_exists($compiledFile1) || file_exists($compiledFile2) || $compiledFile3);
+        (new Compiler)('FakeVendor\HelloWorld', 'prod-cli-app', __DIR__ . '/Fake/fake-app');
+        $this->assertFileExists($compiledFile1);
     }
 }
