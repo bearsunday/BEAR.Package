f676f732dc1c0278397de0dfeeaedea886caf588
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-11-12 11:39:41 +0900

    update context

--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 require dirname(__DIR__) . '/autoload.php';
-exit((require dirname(__DIR__) . '/bootstrap.php')(PHP_SAPI === 'cli-server' ? 'hal-app' : 'prod-hal-app'));
+exit((require dirname(__DIR__) . '/bootstrap.php')('prod-app'));
