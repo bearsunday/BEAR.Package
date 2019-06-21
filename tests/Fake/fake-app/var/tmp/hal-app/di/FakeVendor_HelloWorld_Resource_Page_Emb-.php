cc18ceccfd6a17858e16d86677764491c3a0ac93
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-06-21 16:25:37 +0900

    rm tests/Fake/var/tmp

--- tests/Fake/fake-app/var/tmp/hal-app/di/FakeVendor_HelloWorld_Resource_Page_Emb-.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ray\Di\Compiler;
-
-$instance = new \FakeVendor_HelloWorld_Resource_Page_Emb_IggGcyM();
-$instance->bindings = array('onGet' => array($singleton('BEAR\\Resource\\EmbedInterceptor-')));
-$instance->setRenderer($singleton('BEAR\\Resource\\RenderInterface-'));
-$is_singleton = false;
-return $instance;
