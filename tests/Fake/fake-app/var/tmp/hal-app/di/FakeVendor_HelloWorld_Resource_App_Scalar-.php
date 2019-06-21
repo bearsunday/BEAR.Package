011becfe94f6c12949de0fc8313e989fe62e2367
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-06-22 05:51:54 +0900

    gc

--- tests/Fake/fake-app/var/tmp/hal-app/di/FakeVendor_HelloWorld_Resource_App_Scalar-.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Ray\Di\Compiler;
-
-$instance = new \FakeVendor\HelloWorld\Resource\App\Scalar();
-$instance->setRenderer($singleton('BEAR\\Resource\\RenderInterface-'));
-$is_singleton = false;
-return $instance;
