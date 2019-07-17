86d37a1bee96d74deb5c20c1731b3b64f78a34bb
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-17 17:08:53 +0900

    add Bootstap $app cache key

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -9,6 +9,7 @@ use BEAR\AppMeta\Meta;
 use BEAR\Sunday\Extension\Application\AbstractApp;
 use BEAR\Sunday\Extension\Application\AppInterface;
 use Doctrine\Common\Cache\Cache;
+use function is_string;
 
 final class Bootstrap
 {
@@ -20,15 +21,17 @@ final class Bootstrap
      * @param string $name     application name    'koriym\blog' (vendor\package)
      * @param string $contexts application context 'prod-html-app'
      * @param string $appDir   application path
+     * @param Cache  $cache    cache engine
+     * @param string $cacheKey cache key
      */
-    public function getApp(string $name, string $contexts, string $appDir = '') : AbstractApp
+    public function getApp(string $name, string $contexts, string $appDir = '', Cache $cache = null, string $cacheKey = null) : AbstractApp
     {
-        return $this->newApp(new Meta($name, $contexts, $appDir), $contexts);
+        return $this->newApp(new Meta($name, $contexts, $appDir), $contexts, $cache, $cacheKey);
     }
 
-    public function newApp(AbstractAppMeta $appMeta, string $contexts, Cache $cache = null) : AbstractApp
+    public function newApp(AbstractAppMeta $appMeta, string $contexts, Cache $cache = null, string $cacheKey = null) : AbstractApp
     {
-        $cacheNs = (string) filemtime($appMeta->appDir . '/src');
+        $cacheNs = is_string($cacheKey) ? $cacheKey : (string) filemtime($appMeta->appDir . '/src');
         $injector = new AppInjector($appMeta->name, $contexts, $appMeta, $cacheNs);
         $cache = $cache instanceof Cache ? $cache : $injector->getCachedInstance(Cache::class);
         $appId = $appMeta->name . $contexts . $cacheNs;
