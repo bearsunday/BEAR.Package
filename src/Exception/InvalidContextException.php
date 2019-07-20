85537cc90a0c22f10e54e554408da164c378f82b
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-07-20 17:10:22 +0900

    duplicates of its parent interfaces

--- src/Exception/InvalidContextException.php
+++ src/Exception/InvalidContextException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace BEAR\Package\Exception;
 
-class InvalidContextException extends LogicException implements ExceptionInterface
+class InvalidContextException extends LogicException
 {
 }
--- src/Exception/LocationHeaderRequestException.php
+++ src/Exception/LocationHeaderRequestException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace BEAR\Package\Exception;
 
-class LocationHeaderRequestException extends RuntimeException implements ExceptionInterface
+class LocationHeaderRequestException extends RuntimeException
 {
 }
