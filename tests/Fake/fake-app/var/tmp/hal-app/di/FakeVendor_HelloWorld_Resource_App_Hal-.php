2b3657f0c50026b33d903a3ab5159a9ebcc4685e
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-12-05 03:10:02 +0900

    rm tmp files

--- tests/Fake/fake-app/var/tmp/hal-app/di/FakeVendor_HelloWorld_Resource_App_Hal-.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Ray\Di\Compiler;
-
-$instance = new \FakeVendor\HelloWorld\Resource\App\Hal();
-$instance->setRenderer($prototype('BEAR\\Resource\\RenderInterface-'));
-$is_singleton = false;
-return $instance;
--- tests/Fake/fake-app/var/tmp/hal-app/di/FakeVendor_HelloWorld_Resource_Page_Emb-.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ray\Di\Compiler;
-
-$instance = new \FakeVendor\HelloWorld\Resource\Page\Emb_IggGcyM();
-$instance->bindings = array('onGet' => array($singleton('BEAR\\Resource\\EmbedInterceptor-')));
-$instance->setRenderer($prototype('BEAR\\Resource\\RenderInterface-'));
-$is_singleton = false;
-return $instance;
