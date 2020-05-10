eca50614ed3c17bee5ba193701082116f31e5d00
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-05-10 15:08:29 +0900

    refactor AsycRun

--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -116,8 +116,10 @@ class BootstrapTest extends TestCase
         $testDir = sprintf('%s/tests/Fake', dirname(__DIR__));
         $cmd = sprintf('php %s/boot.php', $testDir);
         file_put_contents($testDir . '/error.log', '');
-        (new AsyncRequest)($cmd);
+        $cmds = array_fill(0, 7, $cmd);
+        $exitCode = (new AsyncRun)($cmds, $testDir . '/error.log');
         // no error should be recorded
         $this->assertSame('', file_get_contents($testDir . '/error.log'));
+        $this->assertSame(0, $exitCode);
     }
 }
--- tests/Fake/boot.php
+++ tests/Fake/boot.php
@@ -8,11 +8,9 @@ use BEAR\Sunday\Extension\Application\AppInterface;
 
 init:
     error_reporting(E_ALL);
-    ini_set('error_log', __DIR__ . '/error.log');
 run:
     $packageDir = dirname(__DIR__, 2);
     require $packageDir . '/vendor/autoload.php';
-    $app = (new Bootstrap)->getApp('FakeVendor\HelloWorld', 'app', $packageDir . '/tests/Fake/fake-app', '');
+    $app = (new Bootstrap)->getApp('FakeVendor\HelloWorld', 'hal-app', $packageDir . '/tests/Fake/fake-app', '');
     $ro = $app->resource->newInstance('/');
-
     exit((int) ! ($app instanceof AppInterface && $ro instanceof ResourceObject));
