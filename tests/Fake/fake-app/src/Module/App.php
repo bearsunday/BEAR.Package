1184cef16e38f09a152f848c534cb3869c2dd391
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-05-12 16:23:09 +0900

    record __construct call number

--- tests/Fake/fake-app/src/Module/App.php
+++ tests/Fake/fake-app/src/Module/App.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace FakeVendor\HelloWorld\Module;
 
+use BEAR\Resource\ResourceInterface;
 use BEAR\Sunday\Extension\Application\AbstractApp;
+use BEAR\Sunday\Extension\Error\ErrorInterface;
+use BEAR\Sunday\Extension\Router\RouterInterface;
+use BEAR\Sunday\Extension\Transfer\HttpCacheInterface;
+use BEAR\Sunday\Extension\Transfer\TransferInterface;
 
 class App extends AbstractApp
 {
+    public static $construct = 0;
+
+    public function __construct(HttpCacheInterface $httpCache, RouterInterface $router, TransferInterface $responder, ResourceInterface $resource, ErrorInterface $error)
+    {
+        parent::__construct($httpCache, $router, $responder, $resource, $error);
+        self::$construct++;
+    }
 }
