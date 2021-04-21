506ac9944c61017186483d8762291ffdb54e9962
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-04-21 19:03:03 +0900

    Fix NullPage cacheable

--- src/Provide/Error/NullPage.php
+++ src/Provide/Error/NullPage.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace BEAR\Package\Provide\Error;
 
 use BEAR\RepositoryModule\Annotation\Cacheable;
+use BEAR\Resource\NullRenderer;
 use BEAR\Resource\ResourceObject;
 
 /**
  * @Cacheable
  */
+#[Cacheable]
 class NullPage extends ResourceObject
 {
-    /** @var int */
-    public $code = 201; // for no template in HTML
+    public function __construct()
+    {
+        $this->renderer = new NullRenderer();
+    }
 
     public function onGet(string $required, int $optional = 0): ResourceObject
     {
