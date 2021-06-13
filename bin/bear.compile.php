27b4afa58ae6e19599a2b25fa87ea1fbff8e20af
Kouhei Sano <apple.x.co@gmail.com>
2021-06-14 08:51:09 +0900

    chore: add memory_limit

--- bin/bear.compile.php
+++ bin/bear.compile.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use BEAR\Package\Compiler;
 
 init:
+    ini_set('memory_limit', '-1');
     $opt = getopt('n:c:d:o');
     [$appName, $context, $appDir] = [$opt['n'], $opt['c'], $opt['d']];
     require realpath($appDir) . '/vendor/autoload.php';
