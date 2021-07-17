f260d1e4d0106fd11615c3dbd2e5646acfb66b1a
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-07-17 14:27:47 +0900

    Change FilesystemAdapter cacheable

--- tests/script/boot.php
+++ tests/script/boot.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use BEAR\AppMeta\Meta;
 use BEAR\Package\Injector;
 use BEAR\Resource\ResourceObject;
 use BEAR\Sunday\Extension\Application\AppInterface;
@@ -12,6 +13,9 @@ require dirname(__DIR__, 2) . '/vendor/autoload.php';
 $opt = getopt('c:n:');
 $context = isset($opt['c']) && is_string($opt['c']) ? $opt['c'] : 'prod-app';
 $appDir = dirname(__DIR__) . '/Fake/fake-app';
+$appName = 'FakeVendor\HelloWorld';
+$meta = new Meta($appName, $context);
+@mkdir($meta->tmpDir . '/di');
 $injector = Injector::getInstance('FakeVendor\HelloWorld', $context, $appDir);
 $app = $injector->getInstance(AppInterface::class);
 assert($app instanceof AppInterface);
