867628a364342f7b464f1ec4da8e2c888785bc9d
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-08-14 11:25:18 +0900

    Update apcu stub for symfony/cache

--- src-files/apcu.php
+++ src-files/apcu.php
@@ -2,52 +2,72 @@
 
 declare(strict_types=1);
 
-if (! function_exists('apcu_store')) {
-    function apcu_cache_info($limited = false)
-    {
-    }
+/**
+ * Apcu stub for machines that do not have ext-apucu installed
+ *
+ * @see \Symfony\Component\Cache\Adapter\ApcuAdapter::isSupported()
+ */
 
-    function apcu_clear_cache()
-    {
-    }
+namespace Symfony\Component\Cache\Adapter {
 
-    function apcu_sma_info($limited = false)
-    {
-    }
+    use function function_exists;
 
-    function apcu_store($key, $var, $ttl = 0)
-    {
+    if (! function_exists('apcu_store')) {
+        function ini_get(string $option)
+        {
+            return $option === 'apc.enabled' ? true : \ini_get($option);
+        }
     }
+}
 
-    function apcu_fetch($key, &$success = null)
-    {
-    }
+namespace {
+    if (! function_exists('apcu_store')) {
+        function apcu_cache_info($limited = false)
+        {
+        }
 
-    function apcu_delete($key)
-    {
-    }
+        function apcu_clear_cache()
+        {
+        }
 
-    function apcu_add($key, $var, $ttl = 0)
-    {
-    }
+        function apcu_sma_info($limited = false)
+        {
+        }
 
-    function apcu_exists($keys)
-    {
-    }
+        function apcu_store($key, $var, $ttl = 0)
+        {
+        }
 
-    function apcu_inc($key, $step = 1, &$success = null)
-    {
-    }
+        function apcu_fetch($key, &$success = null)
+        {
+        }
 
-    function apcu_dec($key, $step = 1, &$success = null)
-    {
-    }
+        function apcu_delete($key)
+        {
+        }
 
-    function apcu_cas($key, $old, $new)
-    {
-    }
+        function apcu_add($key, $var, $ttl = 0)
+        {
+        }
 
-    function apcu_entry($key, callable $generator, $ttl = 0)
-    {
+        function apcu_exists($keys)
+        {
+        }
+
+        function apcu_inc($key, $step = 1, &$success = null)
+        {
+        }
+
+        function apcu_dec($key, $step = 1, &$success = null)
+        {
+        }
+
+        function apcu_cas($key, $old, $new)
+        {
+        }
+
+        function apcu_entry($key, callable $generator, $ttl = 0)
+        {
+        }
     }
 }
