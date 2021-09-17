d44e610a1e8886451b2d734c8d010eb27ab8a286
koriym <akihito.koriyama@gmail.com>
2021-09-17 10:36:42 +0900

    Add BindingsUpdate test

---
+++ tests/Injector/BindingsUpdateTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BEAR\Package\Injector;
+
+use BEAR\AppMeta\Meta;
+use PHPUnit\Framework\TestCase;
+
+use function dirname;
+use function touch;
+
+class BindingsUpdateTest extends TestCase
+{
+    public function testBindingUpdated(): void
+    {
+        $meta = new Meta('FakeVendor\HelloWorld', 'app');
+        $bindingsUpdate = new BindingsUpdate($meta);
+        $isUpdated = $bindingsUpdate->isUpdated($meta);
+        $this->assertFalse($isUpdated);
+
+        touch(dirname(__DIR__) . '/Fake/fake-app/src/Module/AppModule.php');
+        $isUpdated = $bindingsUpdate->isUpdated($meta);
+        $this->assertTrue($isUpdated);
+    }
+}
