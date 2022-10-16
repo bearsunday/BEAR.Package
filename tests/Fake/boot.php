5583e379fd8c1693e46d3b3b85fbbb5ebdbf9f51
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-10-16 09:20:53 +0900

    Update derecated AppInjector

--- tests/Fake/boot.php
+++ tests/Fake/boot.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use BEAR\Package\Bootstrap;
+use BEAR\Package\Injector;
 use BEAR\Resource\ResourceObject;
 use BEAR\Sunday\Extension\Application\AppInterface;
 
@@ -11,6 +12,7 @@ init:
 run:
     $packageDir = dirname(__DIR__, 2);
     require $packageDir . '/vendor/autoload.php';
-    $app = (new Bootstrap)->getApp('FakeVendor\HelloWorld', 'hal-app', $packageDir . '/tests/Fake/fake-app', '');
+    $injector = Injector::getInstance('FakeVendor\HelloWorld', 'hal-app', $packageDir . '/tests/Fake/fake-app');
+    $app = $injector->getInstance(AppInterface::class);
     $ro = $app->resource->newInstance('/');
     exit((int) ! ($app instanceof AppInterface && $ro instanceof ResourceObject));
