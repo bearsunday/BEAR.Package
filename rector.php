dddfe760e4f0078e2c06a6cecdfecea27e8e3716
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-11-19 23:59:41 +0900

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Ray\AnnotationBinding\Rector\ClassMethod\AnnotationBindingRector;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/demo',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/tests-files',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rule(AnnotationBindingRector::class);
+
+    // define sets of rules
+        $rectorConfig->sets([
+            LevelSetList::UP_TO_PHP_80
+        ]);
+};
