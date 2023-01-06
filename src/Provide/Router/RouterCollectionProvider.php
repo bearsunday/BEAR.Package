ff1553457b4c4b6e599fe5eb7d1cf3240325bd57
Akihito Koriyama <akihito.koriyama@gmail.com>
2023-01-06 10:17:43 +0900

    Fix primary_router binding

--- src/Provide/Router/RouterCollectionProvider.php
+++ src/Provide/Router/RouterCollectionProvider.php
@@ -5,16 +5,14 @@ declare(strict_types=1);
 namespace BEAR\Package\Provide\Router;
 
 use BEAR\Sunday\Extension\Router\RouterInterface;
-use Ray\Di\Di\Inject;
 use Ray\Di\Di\Named;
 use Ray\Di\ProviderInterface;
 
 /** @implements ProviderInterface<RouterCollection> */
 class RouterCollectionProvider implements ProviderInterface
 {
-    #[Inject]
     public function __construct(
-        #[Named('router')] private RouterInterface $primaryRouter,
+        #[Named('primary_router')] private RouterInterface $primaryRouter,
         private WebRouterInterface $webRouter,
     ) {
     }
