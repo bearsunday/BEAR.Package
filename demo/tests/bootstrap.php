80583879810b47341f30f20f31264de48bb6d9f3
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-02-08 20:31:36 +0900

    Fix addPsr4 namespace prefix missing trailing backslash

--- demo/tests/bootstrap.php
+++ demo/tests/bootstrap.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 namespace MyVendor\MyProject;
 
 $loader = require dirname(__DIR__, 2) . '/vendor/autoload.php';
-$loader->addPsr4(__NAMESPACE__, dirname(__DIR__) . '/src');
+$loader->addPsr4(__NAMESPACE__ . '\\', dirname(__DIR__) . '/src');
