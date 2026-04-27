d075ff03f7da239a3972d5e088b3324d234d7f98
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-04-28 04:51:27 +0900

    Document cli context exception

--- src/Exception/InvalidCliContextException.php
+++ src/Exception/InvalidCliContextException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace BEAR\Package\Exception;
 
+/** Thrown when the CLI context receives server data without CLI arguments. */
 final class InvalidCliContextException extends InvalidContextException
 {
 }
